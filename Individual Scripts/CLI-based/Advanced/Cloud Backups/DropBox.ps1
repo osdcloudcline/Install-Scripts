3c73f3e9af408731298157d36ff62922dfe9b469
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:00:39 -0400

    Create DropBox.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Cloud Backups/DropBox.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Cloud Backup\$env:computername-DropBox.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "DropBox"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Dropbox.Dropbox --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
