0ad44364f20d88830b685fcfdd777b108fa13951
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:34:02 -0400

    Create AICursorEditor.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Programming/AICursorEditor.ps1
@@ -0,0 +1,14 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Programming\$env:computername-AICursorEditor.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+
+$app1 = "AI Cursor Editor"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id CursorAI,Inc.Cursor --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
