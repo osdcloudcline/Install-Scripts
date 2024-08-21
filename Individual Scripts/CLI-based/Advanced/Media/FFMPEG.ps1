71863db158aeaf235e8e109fb4bca255e33442ae
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:16:52 -0400

    Create FFMPEG.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Media/FFMPEG.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Media\$env:computername-FFMPEG.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "FFMPEG for Audacity"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+choco install audacity-ffmpeg -y
+
+Stop-Transcript
