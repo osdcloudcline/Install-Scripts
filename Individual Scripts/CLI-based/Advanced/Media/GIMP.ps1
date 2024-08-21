7109e51dc92b00e96c649fb06cfbed724e84fb92
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:17:37 -0400

    Create GIMP.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Media/GIMP.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Media\$env:computername-GIMP.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "GIMP"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id GIMP.GIMP --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
