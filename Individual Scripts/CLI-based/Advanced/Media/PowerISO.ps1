a033fef73f5b4bcb3b9f9bbb46ff2eb2e0afeaf9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:24:52 -0400

    Create PowerISO.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Media/PowerISO.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Media\$env:computername-PowerISO.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "PowerISO"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id PowerSoftware.PowerISO --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
