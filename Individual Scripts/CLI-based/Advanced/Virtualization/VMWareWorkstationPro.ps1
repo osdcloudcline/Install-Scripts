e2d3e46ee506fe6e0b5cbe44ddd45f964369b309
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:50:40 -0400

    Create VMWareWorkstationPro.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Virtualization/VMWareWorkstationPro.ps1
@@ -0,0 +1,12 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Virtualization\$env:computername-VMWareWSPro.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "VMWare Workstation Professional"
+
+Write-Host "Installing: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
+choco install vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
+
+Stop-Transcript
