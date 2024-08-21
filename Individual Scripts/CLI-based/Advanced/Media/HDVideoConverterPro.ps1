c5b972a21ffb363b5ba05830dc6eebfc47ab6fb5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:18:38 -0400

    Create HDVideoConverterPro.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Media/HDVideoConverterPro.ps1
@@ -0,0 +1,19 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Media\$env:computername-HDVideoConverterPro.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "HD Video Converter Factory Pro"
+
+Write-Verbose "Processing: $app1..." -Verbose
+Write-Host 
+Write-Host "Acquiring $app1 from vendor website..." -ForegroundColor Cyan
+$HDVideoSource = "https://us.videoconverterfactory.com/download/hd-video-converter-pro.exe"
+$HDVideoDestination = "C:\OSDCloud\GitHub\downloads"
+$HDVideoSetup = "C:\OSDCloud\GitHub\downloads\hd-video-converter-pro.exe"
+Save-WebFile -SourceUrl $HDVideoSource  -DestinationDirectory $HDVideoDestination
+
+Write-Host ' ATTN: Manual install needed!  ' -ForegroundColor DarkRed -BackgroundColor White
+
+Stop-Transcript
