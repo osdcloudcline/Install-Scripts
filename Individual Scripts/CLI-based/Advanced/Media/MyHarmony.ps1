17e25096b97c096b52d96a01a28417c7a7b19845
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:24:03 -0400

    Create MyHarmony.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Media/MyHarmony.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Media\$env:computername-MyHarmony.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Logitech MyHarmony"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Logitech.MyHarmony --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
