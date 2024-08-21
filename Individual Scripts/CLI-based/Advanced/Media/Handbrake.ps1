e1093cc389be3e759a1c39bc51d7dd3b9b4bacaf
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:19:24 -0400

    Create Handbrake.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Media/Handbrake.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Media\$env:computername-Handbrake.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Handbrake"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id HandBrake.HandBrake --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
