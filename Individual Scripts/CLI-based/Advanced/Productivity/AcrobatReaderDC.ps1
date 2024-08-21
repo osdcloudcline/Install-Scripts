0661bb5d95981ee4594c44090b842604838e3739
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 14:28:24 -0400

    Create AcrobatReaderDC.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Productivity/AcrobatReaderDC.ps1
@@ -0,0 +1,8 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Productivity\$env:computername-AcrobatReaderDC.log"
+
+$app1 = "Adobe Acrobat Reader"
+
+Write-Host "Processing: $app1..." -ForegroundColor Cyan
+winget install --id Adobe.Acrobat.Reader.64-bit --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
