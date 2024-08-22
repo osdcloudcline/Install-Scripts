85d124edb80bce0a4c5f5c997b74279ad0ce66fc
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-22 08:20:15 -0400

    Create WinSCP.ps1

---
+++ Individual Scripts/CLI-based/IT Tech/File Transfer/WinSCP.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\IT\File Transfer\$env:computername-WinSCP.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "WinSCP"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id WinSCP.WinSCP --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
