28e896b7b5d2e1e4a0714569d1b76662a8950546
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-22 19:21:03 -0400

    Create Chrome.ps1

---
+++ Individual Scripts/CLI-based/Standard/Browsers/Chrome.ps1
@@ -0,0 +1,14 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Browsers\$env:computername-Chrome.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Google Chrome for Enterprise"
+
+Write-Host
+Write-Host "Installing $app1..." -ForegroundColor Cyan
+winget install --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
+Write-Host
+
+Stop-Transcript
