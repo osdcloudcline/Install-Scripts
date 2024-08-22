92ee557403ea173c1194dd699085a96eda00b8b2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-21 20:38:45 -0400

    Delete GUI-based Scripts/Advanced directory

--- GUI-based Scripts/Advanced/Browsers.ps1
+++
@@ -1,5 +0,0 @@
-$Chrome = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Advanced/GUI%20Scripts/Chrome.ps1")
-Invoke-Expression $($Chrome.Content)
-
-$Firefox = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Advanced/GUI%20Scripts/Firefox.ps1")
-Invoke-Expression $($Firefox.Content)
