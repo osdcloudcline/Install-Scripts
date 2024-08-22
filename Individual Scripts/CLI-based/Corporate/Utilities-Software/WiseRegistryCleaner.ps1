98cafb70d9ba957fc46afef8e47019b505c311d2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-22 01:33:05 -0400

    Create WiseRegistryCleaner.ps1

---
+++ Individual Scripts/CLI-based/Corporate/Utilities-Software/WiseRegistryCleaner.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Corporate\Utilities-Software\$env:computername-WRC.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Wise Registry Cleaner"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id XPDLS1XBTXVPP4 --exact --accept-source-agreements --accept-source-agreements --force
+
+Stop-Transcript
