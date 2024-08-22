0598c0794659a6ab7549de7b7568753525b70f67
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-22 11:24:48 -0400

    Update DockerDesktop.ps1

--- Individual Scripts/CLI-based/IT Tech/Dev Tools/DockerDesktop.ps1
+++ Individual Scripts/CLI-based/IT Tech/Dev Tools/DockerDesktop.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Dev Tools\$env:computername-DockerDesktop.log"
+Start-Transcript -Path "C:\Logs\Powershell\Install\IT\Dev Tools\$env:computername-DockerDesktop.log"
 
 $Date = Get-Date
 
