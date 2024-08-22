72cc556ec705bb187c4a5a14aceee303f5ad9d48
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-22 11:24:31 -0400

    Create GitHubDesktop.ps1

---
+++ Individual Scripts/CLI-based/IT Tech/Dev Tools/GitHubDesktop.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\IT\Dev Tools\$env:computername-GitHubDesktop.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "GitHub Desktop"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id GitHub.GitHubDesktop --exact --accept-source-agreements --accept-source-agreements --force
+
+Stop-Transcript
