5f2353c599de3f49d1895b62f948b372f5b77614
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-22 11:18:05 -0400

    Create DockerDesktop.ps1

---
+++ Individual Scripts/CLI-based/Advanced/Dev Tools/DockerDesktop.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Dev Tools\$env:computername-DockerDesktop.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Docker Desktop"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id Docker.DockerDesktop --exact --accept-source-agreements --accept-source-agreements --force
+
+Stop-Transcript
