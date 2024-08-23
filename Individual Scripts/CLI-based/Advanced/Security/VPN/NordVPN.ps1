c9af84776f817a98952b8f33b324d791a19c0950
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-23 01:38:05 -0400

    Update NordVPN.ps1

--- Individual Scripts/CLI-based/Advanced/Security/VPN/NordVPN.ps1
+++ Individual Scripts/CLI-based/Advanced/Security/VPN/NordVPN.ps1
@@ -1,1 +1,15 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Security\$env:computername-NordVPNDownload.log"
 
+$app1 = "Nord VPN"
+
+Install-Module -Name OSD -AllowClobber -SkipPublisherCheck -Force
+Import-Module -Name OSD -Force
+
+$VPNURL = "https://downloads.nordcdn.com/apps/windows/NordVPN/latest/NordVPNSetup.exe"
+
+$destination = "C:\downloads"
+
+Write-Verbose "Processing $app1 for download..." -Verbose
+Save-WebFile -SourceUrl $VPNURL -DestinationDirectory $destination
+
+Stop-Transcript
