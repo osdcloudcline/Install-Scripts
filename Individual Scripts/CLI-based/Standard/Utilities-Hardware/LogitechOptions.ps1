2b8c6d973448ede07b1a104a8623e298ab7d6255
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-23 00:30:55 -0400

    Create LogitechOptions.ps1

---
+++ Individual Scripts/CLI-based/Standard/Utilities-Hardware/LogitechOptions.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Utilities-Hardware\$env:computername-LogitechOpitions.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Logitech Options"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id Logitech.Options --exact --accept-source-agreements  --accept-package-agreements --force
+
+Stop-Transcript
