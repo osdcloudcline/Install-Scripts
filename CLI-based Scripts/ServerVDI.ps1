4a13e8866e854cf9988b4f772deeda1d049015e7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-23 10:57:55 -0400

    Update ServerVDI.ps1

--- CLI-based Scripts/ServerVDI.ps1
+++ CLI-based Scripts/ServerVDI.ps1
@@ -1,1 +1,20 @@
+Write-Host '                                                                 ' -BackgroundColor White 
+Write-Host '              SERVER VIRTUAL DESKTOP INTERFACE                   ' -ForegroundColor DarkBlue -BackgroundColor White 
+Write-Host '                 SOFTWARE CONFIGURATION                          ' -ForegroundColor DarkBlue -BackgroundColor White 
+Write-Host '          The following software will be installed:              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White 
+Write-Host '  - Web Browsers: DuckDuckGo                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Media: VLC Player and MediaInfo                              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Productivity: Adobe Reader DC                                ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Programming: VS Code, AI Code Editor,                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Utilities: 7-Zip, CCleaner, DDU,                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Utilities: Smart Defrag and Wise Registry Cleaner            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Security: DOWNLOAD ONLY - MalwareBytes, Norton360            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                 ' -BackgroundColor White 
+pause
 
+
+Write-Host "Processing install for: Web Browsers..." -ForegroundColor DarkBlue -BackgroundColor White
+
+$DuckDuckGo = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Virtual%20Desktop%20Interface/Client%20Operating%20Systems/Browsers/DuckDuckGo.ps1")
+Invoke-Expression $($Chrome.Content)
