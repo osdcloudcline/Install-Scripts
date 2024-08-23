5e9f40a95b5ad5d9c3ceda52b4372d42db3b8c61
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-23 09:01:15 -0400

    Update Honeypot.ps1

--- CLI-based Scripts/Honeypot.ps1
+++ CLI-based Scripts/Honeypot.ps1
@@ -2,8 +2,9 @@ Write-Host '                                                                 ' -
 Write-Host '              HONEYPOT SOFTWARE CONFIGURATION                    ' -ForegroundColor DarkBlue -BackgroundColor White 
 Write-Host '          The following software will be installed:              ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '                                                                 ' -BackgroundColor White 
-Write-Host '  - Web Browsers: Google Chrome                                  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - Web Browsers: Google Chrome, DuckDuckGo                      ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '  - Media: VLC Player and MediaInfo                              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - System Utilities: 7-Zip and CCleaner                         ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '  - Security: DOWNLOAD ONLY - IPVanish VPN                       ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '  - Security: DOWNLOAD ONLY - MalwareBytes                       ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '                                                                 ' -BackgroundColor White 
@@ -14,20 +15,31 @@ pause
 
 Write-Host "Processing install for: Web Browsers..." -ForegroundColor DarkBlue -BackgroundColor White
 
-$Browsers = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Browsers/Honeypot/Chrome.ps1")
-Invoke-Expression $($Browsers.Content)
+$Chrome = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Honeypot/Browsers/Chrome.ps1")
+Invoke-Expression $($Chrome.Content)
 
+$DuckDuckGo = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Honeypot/Browsers/DuckDuckGo.ps1")
+Invoke-Expression $($DuckDuckGo.Content)
 
 Write-Host "Processing install for: Media software..." -ForegroundColor DarkBlue -BackgroundColor White
 
-$MediaInfo = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Honetypot/MediaInfo.ps1")
+$MediaInfo = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Honeypot/Media/MediaInfo.ps1")
 Invoke-Expression $($MediaInfo.Content)
 
-$VLC = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Media/Honetypot/VLCPlayer.ps1")
+$VLC = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Honeypot/Media/VLCPlayer.ps1")
+Invoke-Expression $($VLC.Content)
+
+
+Write-Host "Processing install for: System Utilities - Software..." -ForegroundColor DarkBlue -BackgroundColor White
+
+$7Zip = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Honeypot/Utilities-Software/7Zip.ps1")
+Invoke-Expression $($MediaInfo.Content)
+
+$CCleaner = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Honeypot/Utilities-Software/CCleaner.ps1")
 Invoke-Expression $($VLC.Content)
 
 
 Write-Host "Processing downloads for: Security software..." -ForegroundColor DarkBlue -BackgroundColor White
 
-$FileDLs = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/WinGet%20/Install%20/Individual%20Scripts/Security/Honeypot/VMFileDLs.ps1")
+$FileDLs = Invoke-WebRequest ("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Honeypot/Security/VMFileDLs.ps1")
 Invoke-Expression $($FileDLs.Content)
