377862927dd89d0958690841dd199729812fe84e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-23 13:16:13 -0400

    Update Browsers.ps1

--- Individual Scripts/GUI-based/Advanced/Browsers.ps1
+++ Individual Scripts/GUI-based/Advanced/Browsers.ps1
@@ -1,3 +1,4 @@
 winget install --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
 
+winget install --id Mozilla.Firefox --exact --accept-source-agreements  --accept-source-agreements --force
 
