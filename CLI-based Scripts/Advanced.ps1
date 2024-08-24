c23013246ce19c19694b694f15963a2cd25952a9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-24 07:58:54 -0400

    Update Advanced.ps1

--- CLI-based Scripts/Advanced.ps1
+++ CLI-based Scripts/Advanced.ps1
@@ -248,7 +248,7 @@ Invoke-Expression $($DevToolsStoreApps.Content)
 $EntertainmentStoreApps = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Windows%20Store%20Apps/Entertainment-related/EntertainmentStoreApps.ps1")
 Invoke-Expression $($EntertainmentStoreApps.Content)
 
-$NetworkToolsStoreApps = Invoke-WebRequest("")
+$NetworkToolsStoreApps = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Windows%20Store%20Apps/Network%20Tools-related/NetworkToolsStoreApps.ps1")
 Invoke-Expression $($NetworkToolsStoreApps.Content)
 
 $OSStoreApps = Invoke-WebRequest("")
