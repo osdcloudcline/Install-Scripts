79c51c1c6fd45d5e0214e7eb52d9cb92f45df8b9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-25 11:19:15 -0400

    Update DownloadDriver.ps1

--- Individual Scripts/CLI-based/Manufacturer-Specific Scripts/nVidia/Game-Ready Driver/560.94/DownloadDriver.ps1
+++ Individual Scripts/CLI-based/Manufacturer-Specific Scripts/nVidia/Game-Ready Driver/560.94/DownloadDriver.ps1
@@ -4,7 +4,9 @@ Write-Host '  - GeForce RTX 40 Series Game Ready drivers, ver 560.94            
 Write-Host '                                                                     ' -BackgroundColor White 
 
 
-
+####################################
+##  Download File URL
+####################################
 
 $GF40SeriesURL = "https://us.download.nvidia.com/Windows/560.94/560.94-desktop-win10-win11-64bit-international-dch-whql.exe"
 
