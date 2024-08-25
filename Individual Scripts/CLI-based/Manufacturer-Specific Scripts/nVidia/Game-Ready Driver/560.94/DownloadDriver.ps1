5b223960ad64f781f512895541d7e8942029fbae
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-25 10:50:55 -0400

    Update DownloadDriver.ps1

--- Individual Scripts/CLI-based/Manufacturer-Specific Scripts/nVidia/Game-Ready Driver/560.94/DownloadDriver.ps1
+++ Individual Scripts/CLI-based/Manufacturer-Specific Scripts/nVidia/Game-Ready Driver/560.94/DownloadDriver.ps1
@@ -1,1 +1,11 @@
-https://us.download.nvidia.com/Windows/560.94/560.94-desktop-win10-win11-64bit-international-dch-whql.exe
+Write-Host '                                                                     ' -BackgroundColor White 
+Write-Host '  The following nVidia GeForce RTX 4090 drivers will be downloaded:  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '  - GeForce RTX 40 Series Game Ready drivers, ver 560.94             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                     ' -BackgroundColor White 
+
+
+
+
+$GF40SeriesURL = "https://us.download.nvidia.com/Windows/560.94/560.94-desktop-win10-win11-64bit-international-dch-whql.exe"
+
+
