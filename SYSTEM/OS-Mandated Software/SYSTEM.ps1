d1a743467dfe678ea81746e10160f778acc88582
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-04 11:39:47 -0400

    Update SYSTEM.ps1

--- SYSTEM/OS-Mandated Software/SYSTEM.ps1
+++ SYSTEM/OS-Mandated Software/SYSTEM.ps1
@@ -23,6 +23,24 @@ $NETapp5ver = "Version: 8.0.1"
 $NETapp6 = ".NET Framework"
 $NETapp6ver = "Version: 4.8.1"
 
+$SDKApps1 = ".NET Framework SDK"
+$SDKApp1Ver = "Version: 8.0"
+
+$ASPapp1 = ".NET ASP Core"
+$ASPapp1ver = "Version: 3.1.32"
+
+$ASPapp2 = ".NET ASP Core"
+$ASPapp2ver = "Version: 5.0.17"
+
+$ASPapp3 = ".NET ASP Core"
+$ASPapp3ver = "Version: 6.0.33"
+
+$ASPapp4 = ".NET ASP Core"
+$ASPapp4ver = "Version: 7.0.20"
+
+$ASPapp5 = ".NET ASP Core"
+$ASPapp5ver = "Version: 8.0.8"
+
 $VCRedistributableapp1 = "MS VS C++ Redistributable 2005 x64"
 $VCRedistributableapp1ver = "Version: 8.0.61000"
 
@@ -97,31 +115,63 @@ $SYSGroup6 = "OS Software"
 Write-Host "Installing Chocolatey..." -ForegroundColor Cyan 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+#SDK Region
+
+Write-Host "Installing $SYSGroup1...." -ForegroundColor Cyan
+Write-Host
+Write-Verbose "Installing $SDKapp1, $SDKApp1Ver..." -Verbose
+winget install --id Microsoft.DotNet.SDK.8 --exact --accept-source-agreements --accept-source-agreements --force
+
+#ASP Region 
+
+Write-Host "Installing $SYSGroup1...." -ForegroundColor Cyan
+Write-Host
+
+Write-Host
+Write-Verbose "Installing $ASPapp1, $ASPApp1Ver..." -Verbose
+winget install --id Microsoft.DotNet.AspNetCore.3_1 --accept-source-agreements --accept-source-agreements --force
+
+Write-Host
+Write-Verbose "Installing $ASPapp2, $ASPApp2Ver..." -Verbose
+winget install --id Microsoft.DotNet.AspNetCore.5 --accept-source-agreements --accept-source-agreements --force
+
+
+Write-Host
+Write-Verbose "Installing $ASPapp3, $ASPApp3Ver..." -Verbose
+winget install --id Microsoft.DotNet.AspNetCore.6 --accept-source-agreements --accept-source-agreements --force
+
+Write-Host
+Write-Verbose "Installing $ASPapp4, $ASPApp4Ver..." -Verbose
+winget install --id Microsoft.DotNet.AspNetCore.7 --accept-source-agreements --accept-source-agreements --force
+
+
+Write-Host
+Write-Verbose "Installing $ASPapp5, $ASPApp5Ver..." -Verbose
+winget install --id Microsoft.DotNet.AspNetCore.8 --accept-source-agreements --accept-source-agreements --force
 # NET Region
 
 Write-Host "Installing $SYSGroup1...." -ForegroundColor Cyan
 Write-Host
-Write-Host "Installing $SYSGroup1....1 of 6" -ForegroundColor Cyan
 Write-Verbose "Installing $NETapp1, $NETapp1ver..." -Verbose
 winget install --id  Microsoft.DotNet.Runtime.3_1 --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing $SYSGroup1....2 of 6" -ForegroundColor Cyan
+Write-Host
 Write-Verbose "Installing $NETapp2, $NETapp2ver..." -Verbose
 winget install --id Microsoft.DotNet.Runtime.5 --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing $SYSGroup1....3 of 6" -ForegroundColor Cyan
+Write-Host
 Write-Verbose "Installing $NETapp3, $NETapp3ver..." -Verbose
 winget install --id Microsoft.DotNet.Runtime.6 --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing $SYSGroup1....4 of 6" -ForegroundColor Cyan
+Write-Host
 Write-Verbose "Installing $NETapp4, $NETapp4ver..." -Verbose
 winget install --id Microsoft.DotNet.Runtime.7 --exact --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing $SYSGroup1....5 of 6" -ForegroundColor Cyan
+Write-Host
 Write-Verbose "Installing $NETapp5, $NETapp5ver..." -Verbose
 winget install --id Microsoft.DotNet.Runtime.8 --accept-source-agreements --accept-source-agreements --force
 
-Write-Host "Installing $SYSGroup1....6 of 6" -ForegroundColor Cyan
+Write-Host
 Write-Verbose "Installing $NETapp6, $NETapp6ver..." -Verbose 
 choco install dotnetfx -y
 
