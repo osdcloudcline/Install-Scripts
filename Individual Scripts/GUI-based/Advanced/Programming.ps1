5135a3bcab8e01e3f09b84dc613dcd226e8ea879
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-05 08:14:08 -0400

    Update Programming.ps1

--- Individual Scripts/GUI-based/Advanced/Programming.ps1
+++ Individual Scripts/GUI-based/Advanced/Programming.ps1
@@ -1,1 +1,11 @@
+$ProgrammingInstallLog = "C:\Logs\Installs\Software\Advanced\GUI\$env:computername-Programming.log"
 
+Start-Transcript -Path $ProgrammingInstallLog
+
+winget install --id CursorAI,Inc.Cursor --exact --accept-source-agreements  --accept-source-agreements --force
+
+choco install visualstudio2022enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
+
+winget install --id Microsoft.VisualStudioCode --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
