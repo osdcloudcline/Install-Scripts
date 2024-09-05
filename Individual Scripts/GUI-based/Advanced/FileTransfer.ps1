78c7e1dbee552c480cc56d803c90848ba0448249
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-05 00:31:57 -0400

    Update FileTransfer.ps1

--- Individual Scripts/GUI-based/Advanced/FileTransfer.ps1
+++ Individual Scripts/GUI-based/Advanced/FileTransfer.ps1
@@ -1,5 +1,5 @@
+$FileTransferInstallLog = "C:\Logs\Installs\Software\Advanced\GUI\$env:computername-FileTransfer.log"
 
-
-
+Start-Transcript -Path $FileTransferInstallLog
 
 choco install filezilla  -y
