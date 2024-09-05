7b43ee26c7706004c28c481f09d6b71feb404c4f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-05 07:48:26 -0400

    Update Gaming.ps1

--- Individual Scripts/GUI-based/Advanced/Gaming.ps1
+++ Individual Scripts/GUI-based/Advanced/Gaming.ps1
@@ -1,3 +1,7 @@
 $GamingInstallLog = "C:\Logs\Installs\Software\Advanced\GUI\$env:computername-Gaming.log"
 
 Start-Transcript -Path $GamingInstallLog
+
+winget install --id Valve.Steam --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
