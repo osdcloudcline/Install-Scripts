c8c57e088de3291ea0d9b0a7cc04a37ea14b6802
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-05 09:20:34 -0400

    Update Security.ps1

--- Individual Scripts/GUI-based/Advanced/Security.ps1
+++ Individual Scripts/GUI-based/Advanced/Security.ps1
@@ -1,1 +1,22 @@
+$SecurityDownloadLog = ""
 
+Start-Transcript $SecurityDownloadLog
+
+Install-Module -Name OSD -AllowClobber -SkipPublisherCheck -Force
+Import-Module -Name OSD -Force
+
+$MBDLURL = "https://github.com/osdcloudcline/Software/raw/main/Security/MalwareBytes/MBSetup.exe"
+
+$destination = "C:\downloads"
+
+Save-WebFile -SourceUrl $MBDLURL -DestinationDirectory $destination
+
+$BDURLDL = "https://github.com/osdcloudcline/Software/raw/main/Security/BitDefender/bitdefender_tsecurity.exe"
+
+Save-WebFile -SourceUrl $BDURLDL -DestinationDirectory $destination
+
+$N360DLURL = "https://github.com/osdcloudcline/Software/raw/main/Security/Norton%20360/N360Downloader.exe"
+
+Save-WebFile -SourceUrl $N360DLURL -DestinationDirectory $destination
+
+Stop-Transcript
