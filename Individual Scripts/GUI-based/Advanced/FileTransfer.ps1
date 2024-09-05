d099b9d6765dd1a51cf5897d5e06e7d542f4b442
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-05 00:32:28 -0400

    Update FileTransfer.ps1

--- Individual Scripts/GUI-based/Advanced/FileTransfer.ps1
+++ Individual Scripts/GUI-based/Advanced/FileTransfer.ps1
@@ -3,3 +3,6 @@ $FileTransferInstallLog = "C:\Logs\Installs\Software\Advanced\GUI\$env:computern
 Start-Transcript -Path $FileTransferInstallLog
 
 choco install filezilla  -y
+
+winget install --id PuTTY.PuTTY --exact --accept-source-agreements --architecture x64  --accept-source-agreements --force
+
