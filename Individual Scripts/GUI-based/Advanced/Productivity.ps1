0bd57a6e9e02f6bb887721bb0a539a58624fede3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-05 08:11:39 -0400

    Update Productivity.ps1

--- Individual Scripts/GUI-based/Advanced/Productivity.ps1
+++ Individual Scripts/GUI-based/Advanced/Productivity.ps1
@@ -4,21 +4,8 @@ Start-Transcript -Path $ProductivityInstallLog
 
 winget install --id Adobe.Acrobat.Reader.64-bit --exact --accept-source-agreements  --accept-source-agreements --force
 
-$OfficeGHdownloads = "C:\downloads\Office" 
-mkdir $OfficeGHdownloads
-$Office2021FilesUrl = "https://github.com/osdcloudcline/Software/raw/main/Productivity/Microsoft/Office%202021/OSDCloud-Office2021.zip"
-Save-WebFile -SourceUrl $Office2021FilesUrl -DestinationDirectory $OfficeGHdownloads
-
-$O21DLCMD = "C:\downloads\Office\OfficeCDNDownload.cmd"
-Start-Process -FilePath $O21DLCMD
-
 winget install --id 9WZDNCRD29V9 --source msstore
 
-Expand-Archive -Path "$OfficeGHdownloads\OSDCloud-Office2021.zip" -DestinationPath $OfficeGHdownloads
-
 winget install --id Microsoft.Office --exact --accept-source-agreements  --accept-source-agreements --force
-
-$O21SetupCMD = "C:\downloads\Office\OfficeCDNSetup.cmd"
-Start-Process -FilePath $O21SetupCMD
 
 Stop-Transcript
