593ea600602a95f7917d5f57feee42c82472f88b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-05 00:28:57 -0400

    Update Browsers.ps1

--- Individual Scripts/GUI-based/Advanced/Browsers.ps1
+++ Individual Scripts/GUI-based/Advanced/Browsers.ps1
@@ -1,4 +1,11 @@
+$BrowsersInstallLog = "C:\Logs\Installs\Software\Advanced\GUI\$env:computername-Browsers.log"
+
+Start-Transcript -Path $BrowsersInstallLog
+
 winget install --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
 
 winget install --id Mozilla.Firefox --exact --accept-source-agreements  --accept-source-agreements --force
 
+winget install --id DuckDuckGo.DesktopBrowser --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
