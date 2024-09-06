4078f584a9891c7afa5e0f66e65846fc11626a63
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-06 18:05:59 -0400

    Create CloudBackups.ps1

---
+++ Individual Scripts/GUI-based/IT Tech/CloudBackups.ps1
@@ -0,0 +1,15 @@
+$CloudBackupsInstallLog = "C:\Logs\Installs\Software\IT\GUI\$env:computername-CloudBackups.log"
+
+Start-Transcript -Path $CloudBackupsInstallLog
+
+winget install --id Dropbox.Dropbox --source winget --force
+
+winget install --id Google.GoogleDrive --source winget --force
+
+winget install --id Nextcloud.NextcloudDesktop --source winget --force
+
+winget install --id Microsoft.OneDrive --source winget --force
+
+winget install --id Apple.iCloud --source winget --force
+
+Stop-Transcript
