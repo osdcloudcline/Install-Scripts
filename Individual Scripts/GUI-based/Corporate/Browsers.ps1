a4e5b1d7889531c9c3f63df82ddcb286605d3e70
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-05 23:38:46 -0400

    Create Browsers.ps1

---
+++ Individual Scripts/GUI-based/Corporate/Browsers.ps1
@@ -0,0 +1,9 @@
+$BrowsersInstallLog = "C:\Logs\Installs\Software\Corporate\GUI\$env:computername-Browsers.log"
+
+Start-Transcript -Path $BrowsersInstallLog
+
+winget install --id Google.Chrome --exact --accept-source-agreements  --accept-source-agreements --force
+
+winget install --id DuckDuckGo.DesktopBrowser --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
