7ce7cde221f5d910e2d0feb8eae53f74518376b5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-06 18:17:18 -0400

    Create RemoteConnections.ps1

---
+++ Individual Scripts/GUI-based/IT Tech/RemoteConnections.ps1
@@ -0,0 +1,11 @@
+$RemoteConnectionsInstallLog = "C:\Logs\Installs\Software\IT\GUI\$env:computername-RemoteConnections.log"
+
+Start-Transcript -Path $RemoteConnectionsInstallLog
+
+winget install --id AnyDeskSoftwareGmbH.AnyDesk --exact --accept-source-agreements  --accept-source-agreements --force
+
+winget install --id Microsoft.RemoteDesktopClient --exact --accept-source-agreements  --accept-source-agreements --force
+
+choco install vmware-horizon-client -y
+
+Stop-Transcript
