a00d3d5a04cad046982c5ad0f991edc5dfe9137f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-05 23:41:20 -0400

    Create Media.ps1

---
+++ Individual Scripts/GUI-based/Corporate/Media.ps1
@@ -0,0 +1,9 @@
+$MediaInstallLog = "C:\Logs\Installs\Software\Corporate\GUI\$env:computername-Media.log"
+
+Start-Transcript -Path $MediaInstallLog
+
+winget install --id MediaArea.MediaInfo.GUI --exact --accept-source-agreements  --accept-source-agreements --force
+
+winget install --id VideoLAN.VLC --exact --accept-source-agreements  --accept-source-agreements --force
+
+Stop-Transcript
