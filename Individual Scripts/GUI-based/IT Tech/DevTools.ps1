0fb543d24826deb8110f8415f008b27a1fee7603
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-09-06 18:07:29 -0400

    Create DevTools.ps1

---
+++ Individual Scripts/GUI-based/IT Tech/DevTools.ps1
@@ -0,0 +1,9 @@
+$DevToolsInstallLog = "C:\Logs\Installs\Software\IT\GUI\$env:computername-DevTools.log"
+
+Start-Transcript -Path $DevToolsInstallLog
+
+winget install --id Docker.DockerDesktop --source winget --force
+
+winget install --id GitHub.GitHubDesktop --source winget --force
+
+Stop-Transcript
