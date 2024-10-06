725b3e98bb811eb377e9bc088022a412e25c2f47
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-06 10:04:29 -0400

    Update Lian-Li.ps1

--- Individual Scripts/CLI-based/Manufacturer-Specific Scripts/Lian-Li/Lian-Li.ps1
+++ Individual Scripts/CLI-based/Manufacturer-Specific Scripts/Lian-Li/Lian-Li.ps1
@@ -7,8 +7,8 @@ $MfrSoftwareGroup1 = "Lian-Li"
 Write-Host "Installing $MfrSoftwareGroup1 Software..." -ForegroundColor Cyan
 Write-Host
 Write-Host
-Write-Verbose "Installing: $LianLiSoftware1...OEM Software No: 1 of 2" -Verbose
-$LConnect = Invoke-WebRequest("")
+Write-Verbose "Installing: $LianLiSoftware1..." -Verbose
+$LConnect = Invoke-WebRequest("https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/Lian-Li/L-Connect%203/v%202.0.18/LConnect3.ps1")
 Invoke-Expression $($LConnect.Content)
-Write-Verbose "Installing: $FanSoftware1...OEM Software No: 2 of 2" -Verbose
+Write-Verbose "Installing: $FanSoftware1..." -Verbose
 winget.exe install --id Rem0o.FanControl --exact --accept-source-agreements  --accept-package-agreements --force
