0d9845209980f74d0ecedd91ebf17c773a73961b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-06 08:49:42 -0400

    Update DockerImage-Windows.ps1

--- GUI-based Scripts/DockerImage-Windows.ps1
+++ GUI-based Scripts/DockerImage-Windows.ps1
@@ -1,2 +1,25 @@
 $DOCKERprocesses = @(
   @{
+        Name = "Processing: Microsoft Windows PowerShell Providers"
+    Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Package%20Providers/Install.ps1"
+    },
+  @{
+    Name = "Processing: Microsoft Windows PowerShell 5.1 Modules"
+    Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%205.1/Install.ps1"
+    },
+  @{
+   Name = "Processing: Microsoft Windows PowerShell 7.1 Modules"
+   Script = "https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%207.x/Install.ps1"
+   },
+  @{
+  Name = "Processing: Client Operating System SYSTEM Registry Entries - Windows Safe Mode"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/AddSafeMode.ps1"
+  },
+  @{
+  Name = "Processing: Client Operating System SYSTEM Registry Entries - Check For Updates"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/CheckForUpdates.ps1"
+  },
+  @{
+  Name = "Processing: Client Operating System SYSTEM Registry Entries - CopyTo/MoveTo"
+  Script = "https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/Client%20Operating%20System/Scripts/CopyToMoveTo.ps1"
+  },
