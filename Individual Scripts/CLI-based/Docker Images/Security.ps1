42e086542ee25a6e4d481617e3cd2ce62b753202
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-06 10:09:26 -0400

    Update Security.ps1

--- Individual Scripts/CLI-based/Docker Images/Security.ps1
+++ Individual Scripts/CLI-based/Docker Images/Security.ps1
@@ -1,1 +1,15 @@
+$DockerSecurityLog = "C:\Logs\DockerImages\Security.log"
 
+$MBAMURL = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Security/MalwareBytes/MBSetup.exe"
+$destination = "C:\download"
+
+Start-Transcript -Path $DockerSecurityLog
+
+Get-Date
+
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+Save-WebFile -SourceUrl $MBAMURL -DestinationDirectory $destination
+
+Stop-Transcript
