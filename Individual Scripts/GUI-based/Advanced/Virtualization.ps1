8690535afaba3aa672fc81316804ff7affa49f0e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-17 08:59:22 -0400

    Update Virtualization.ps1

--- Individual Scripts/GUI-based/Advanced/Virtualization.ps1
+++ Individual Scripts/GUI-based/Advanced/Virtualization.ps1
@@ -20,5 +20,13 @@ Write-Host
 Write-Host "Extracting: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
 Expand-7Zip "C:\downloads\VMware-workstation-17.6.1-24319023.exe.tar" $destination
 
+
+$Filelocation = "C:\downloads\VMware-workstation-17.6.1-24319023.exe"
+
+Write-Verbose "Installing VMWare Workstation Professional 17.6.1 Build 24319023..." -Verbose
+Write-Host
+Start-Process -FilePath $Filelocation -ArgumentList '/s','/v"/qn"'
+
 Stop-Transcript
+
 
