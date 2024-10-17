2afa3b3fb349dd724599e5ef96a2e715212394e8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-17 08:53:59 -0400

    Update VMWareWorkstationPro.ps1

--- Individual Scripts/CLI-based/Advanced/Virtualization/VMWareWorkstationPro.ps1
+++ Individual Scripts/CLI-based/Advanced/Virtualization/VMWareWorkstationPro.ps1
@@ -9,7 +9,7 @@ $app1 = "VMWare Workstation Professional"
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
 
-$VMWareWSProDLURL = "https://softwareupdate.vmware.com/cds/vmw-desktop/ws/17.6.0/24238078/windows/core/VMware-workstation-17.6.0-24238078.exe.tar"
+$VMWareWSProDLURL = "https://softwareupdate.vmware.com/cds/vmw-desktop/ws/17.6.1/24319023/windows/core/VMware-workstation-17.6.1-24319023.exe.tar"
 $destination = "C:\downloads"
 
 
@@ -18,7 +18,7 @@ Save-WebFile -SourceUrl $VMWareWSProDLURL -DestinationDirectory $destination
 
 Write-Host 
 Write-Host "Extracting: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
-Expand-7Zip "C:\downloads\VMware-workstation-17.6.0-24238078.exe.tar" $destination
+Expand-7Zip "C:\downloads\VMware-workstation-17.6.1-24319023.exe.tar" $destination
 
 Stop-Transcript
 
