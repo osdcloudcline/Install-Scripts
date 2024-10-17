8acd1d5bc5d4a3918ee5ccbcfe39222fbd92ad23
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-17 08:55:05 -0400

    Update InstallVMWarePro.ps1

--- Individual Scripts/CLI-based/Advanced/Virtualization/InstallVMWarePro.ps1
+++ Individual Scripts/CLI-based/Advanced/Virtualization/InstallVMWarePro.ps1
@@ -1,5 +1,5 @@
-$Filelocation = "C:\downloads\VMware-workstation-17.6.0-24238078.exe"
+$Filelocation = "C:\downloads\VMware-workstation-17.6.1-24319023.exe"
 
-Write-Verbose "Installing VMWare Workstation Professional 17.6 Build 24238078..." -Verbose
+Write-Verbose "Installing VMWare Workstation Professional 17.6.1 Build 24319023..." -Verbose
 Write-Host
 Start-Process -FilePath $Filelocation -ArgumentList '/s','/v"/qn"'
