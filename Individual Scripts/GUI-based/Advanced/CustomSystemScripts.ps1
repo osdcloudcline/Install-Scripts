757dfa5436f915c7d53f40b7d449fd70d0b4240f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-18 08:20:13 -0400

    Update CustomSystemScripts.ps1

--- Individual Scripts/GUI-based/Advanced/CustomSystemScripts.ps1
+++ Individual Scripts/GUI-based/Advanced/CustomSystemScripts.ps1
@@ -3,8 +3,12 @@ $CustomScriptLogs = ""
 Start-Transcript -Path $CustomScriptLogs
 
 If ($env:computername -eq "BTDESKTOP"){
+
 $LianLi = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Individual%20Scripts/GUI-based/System-Specific%20Scripts/Desktop%20Computers/Bryan%20Desktop/Lian-Li.ps1")
 Invoke-Expression $($LianLi.Content)
+
+$SignalRGB = Invoke-WebRequest("https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/SignalRGB.ps1")
+Invoke-Expression $($SignalRGB.Content)
 
 
 }
