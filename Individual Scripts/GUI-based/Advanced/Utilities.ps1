eb7a1b43847c615448c28da83c30d5c082220d3f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-18 07:53:58 -0400

    Update Utilities.ps1

--- Individual Scripts/GUI-based/Advanced/Utilities.ps1
+++ Individual Scripts/GUI-based/Advanced/Utilities.ps1
@@ -69,8 +69,9 @@ $VMWareToolsdestination = "C:\downloads"
 Save-WebFile -SourceUrl $VMWareToolsURL -DestinationDirectory $VMWareToolsdestination
 
 If ($env:computername -eq "BTDESKTOP"){
-$LianLi = Invoke-WebRequest("")
+$LianLi = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Individual%20Scripts/GUI-based/System-Specific%20Scripts/Desktop%20Computers/Bryan%20Desktop/Lian-Li.ps1")
 Invoke-Expression $($LianLi.Content)
+
 
 }
 
