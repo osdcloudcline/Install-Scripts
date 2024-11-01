1b3ae21e2f598d6989b8e03b359fbad19e053d9b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-31 22:48:29 -0400

    Update SignalRGB.ps1

--- Individual Scripts/GUI-based/System-Specific Scripts/Desktop Computers/Bryan Desktop/SignalRGB.ps1
+++ Individual Scripts/GUI-based/System-Specific Scripts/Desktop Computers/Bryan Desktop/SignalRGB.ps1
@@ -1,1 +1,2 @@
-$SignalRGBURL = ""
+$SignalRGBURL = Invoke-WebRequest( "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/SignalRGB.ps1")
+Invoke-Expression $($SignalRGB.Content)
