fb979ca72b1c006c0e89a6cf26c1eb77a6976b00
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-12 00:47:33 -0500

    Update StreamDeckBackup.ps1

--- Individual Scripts/GUI-based/System-Specific Scripts/Desktop Computers/Bryan Desktop/StreamDeckBackup.ps1
+++ Individual Scripts/GUI-based/System-Specific Scripts/Desktop Computers/Bryan Desktop/StreamDeckBackup.ps1
@@ -1,1 +1,11 @@
 
+
+
+Import-Module -Name OSD -Force
+
+$StreamDeckProfileURL = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Other/Stream%20Deck%20-%2016-10-2024%20-%2006-44.streamDeckProfilesBackup"
+$Destination = "C:\downloads"
+
+
+Write-Verbose "Processing: Acquiring Stream Deck Profile from OSDCloudCline GitHub repo..."
+Save-WebFile -SourceUrl $StreamDeckProfileURL  -DestinationDirectory 
