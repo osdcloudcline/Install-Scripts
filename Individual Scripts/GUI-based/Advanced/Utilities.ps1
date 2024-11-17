8b418ca5981199e589cd8fdb37fbcdc6ea8e33d5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-17 14:05:43 -0500

    Update Utilities.ps1

--- Individual Scripts/GUI-based/Advanced/Utilities.ps1
+++ Individual Scripts/GUI-based/Advanced/Utilities.ps1
@@ -27,11 +27,14 @@ winget install --id Wagnardsoft.DisplayDriverUninstaller --exact --accept-source
 winget install --id Glarysoft.GlaryUtilities --exact --accept-source-agreements  --accept-package-agreements --force
 
 $OSDCloudGHdownloads = "C:\downloads"
-$GlaryZipURL = ""
-$GlaryUnZipPath = ""
+$GlaryZipURL1 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/Glary%20Utilities/GlaryUtilities1.zip"
+$GlaryZipURL2 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/Glary%20Utilities/GlaryUtilities2.zip"
+$GlaryUnZipPath = "C:\downloads\GlaryUtilities"
 
-Save-WebFile -SourceUrl $GlaryZipURL -DestinationDirectory $OSDCloudGHdownloads
-Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities.zip" -TargetPath $GlaryUnZipPath 
+Save-WebFile -SourceUrl $GlaryZipURL1 -DestinationDirectory $OSDCloudGHdownloads
+Save-WebFile -SourceUrl $GlaryZipURL2 -DestinationDirectory $OSDCloudGHdownloads
+Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities1.zip" -TargetPath $GlaryUnZipPath 
+Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities2.zip" -TargetPath $GlaryUnZipPath 
 Copy-Item -Path $GlaryUnZipPath -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
 
 $OSDCloudGHdownloads = "C:\downloads"
