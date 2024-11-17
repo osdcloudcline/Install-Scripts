adc4db5d2c2b5082b40181e2fa4693c82b82a466
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-17 14:38:36 -0500

    Update GlaryUtilities.ps1

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/GlaryUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/GlaryUtilities.ps1
@@ -22,6 +22,7 @@ Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities1.zip" -TargetP
 Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities2.zip" -TargetPath $GlaryUnZipPath 
 Copy-Item -Path "C:\downloads\GlaryUtilities" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
 Stop-Process -Name Integrator.exe -Force
-
+Stop-Process -Name MemfilesService.exe -Force
 Copy-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
+Remove-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities" -Force -Confirm:$false
 Stop-Transcript
