491b00ed3b4e3bc8071896177b2f0762a0898a5a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-17 15:43:23 -0500

    Update GlaryUtilities.ps1

--- Individual Scripts/CLI-based/IT Tech/Utilities-Software/GlaryUtilities.ps1
+++ Individual Scripts/CLI-based/IT Tech/Utilities-Software/GlaryUtilities.ps1
@@ -20,10 +20,10 @@ Save-WebFile -SourceUrl $GlaryZipURL1 -DestinationDirectory $OSDCloudGHdownloads
 Save-WebFile -SourceUrl $GlaryZipURL2 -DestinationDirectory $OSDCloudGHdownloads
 Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities1.zip" -TargetPath $GlaryUnZipPath 
 Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities2.zip" -TargetPath $GlaryUnZipPath 
-Copy-Item -Path "C:\downloads\GlaryUtilities\*.*" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
+Copy-Item -Path "C:\downloads\GlaryUtilities" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
 Stop-Process -Name Integrator.exe -Force
 Stop-Process -Name MemfilesService.exe -Force
-Copy-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
+Copy-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities\*.*" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
 Remove-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities" -Force -Confirm:$false
 Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Glary Utilities.lnk" -Destination "C:\Users\$env:username\Desktop" -Force
 Stop-Transcript
