c6ea7c64f3d9cb6f36353e9eb3f4a5a6d909d515
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-17 13:57:23 -0500

    Update Utilities.ps1

--- Individual Scripts/GUI-based/Advanced/Utilities.ps1
+++ Individual Scripts/GUI-based/Advanced/Utilities.ps1
@@ -24,6 +24,16 @@ winget install --id Piriform.CCleaner --exact --accept-source-agreements  --acce
 
 winget install --id Wagnardsoft.DisplayDriverUninstaller --exact --accept-source-agreements  --accept-package-agreements --force
 
+winget install --id Glarysoft.GlaryUtilities --exact --accept-source-agreements  --accept-package-agreements --force
+
+$OSDCloudGHdownloads = "C:\downloads"
+$GlaryZipURL = ""
+$GlaryUnZipPath = ""
+
+Save-WebFile -SourceUrl $GlaryZipURL -DestinationDirectory $OSDCloudGHdownloads
+Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities.zip" -TargetPath $GlaryUnZipPath 
+Copy-Item -Path $GlaryUnZipPath -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
+
 $OSDCloudGHdownloads = "C:\downloads"
 $Win11_22H2SDK = "C:\downloads\winsdksetup.exe"
 $Win11_22H2SDKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/winsdksetup.exe'
