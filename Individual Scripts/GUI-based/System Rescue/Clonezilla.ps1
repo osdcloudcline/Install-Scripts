1ffe410de4892fc8cd8dcf41dc4a382fc1caf21d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-23 18:45:29 -0500

    Create Clonezilla.ps1

---
+++ Individual Scripts/GUI-based/System Rescue/Clonezilla.ps1
@@ -0,0 +1,27 @@
+$SystemRescueLog = "C:\Logs\OSDCloud\Install\SystemRescue\Clonezilla.log"
+
+Start-Transcript -Path $SystemRescueLog
+
+Get-Date
+
+Import-Module -Name OSD -Force
+
+
+##############################
+## Clonezilla URL ######
+##############################
+
+$ClonezillaURL = "https://sourceforge.net/projects/clonezilla/files/clonezilla_live_stable/3.2.0-5/clonezilla-live-3.2.0-5-amd64.iso/download"
+
+##############################
+## Download destination 
+##############################
+
+$DLDestination = "C:\downloads\SystemRescue"
+
+
+Write-Verbose "Processing: Downloading Clonezilla x64 3.2.0-5..."
+
+Save-WebFile -SourceUrl $ClonezillaURL -DestinationDirectory $DLDestination
+
+Stop-Transcript
