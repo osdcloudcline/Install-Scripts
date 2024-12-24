e8b05b0abe14351adc34b74068e5af652628776c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-23 19:13:13 -0500

    Update Office2021GHDownload.ps1

--- Individual Scripts/GUI-based/Advanced/Office2021GHDownload.ps1
+++ Individual Scripts/GUI-based/Advanced/Office2021GHDownload.ps1
@@ -1,7 +1,7 @@
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
 
-$OfficeGHdownloads = "C:\downloads\Office" 
+$OfficeGHdownloads = "C:\downloads\Office\2021" 
 mkdir $OfficeGHdownloads
 $Office2021FilesUrl = "https://github.com/osdcloudcline/Software/raw/main/Productivity/Microsoft/Office%202021/OSDCloud-Office2021.zip"
 Save-WebFile -SourceUrl $Office2021FilesUrl -DestinationDirectory $OfficeGHdownloads
