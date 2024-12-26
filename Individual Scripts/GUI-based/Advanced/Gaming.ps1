2bf52bb1b7d6e23c41df0e84c8d1dc8c61420aa6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-26 01:13:32 -0500

    Update Gaming.ps1

--- Individual Scripts/GUI-based/Advanced/Gaming.ps1
+++ Individual Scripts/GUI-based/Advanced/Gaming.ps1
@@ -4,4 +4,8 @@ Start-Transcript -Path $GamingInstallLog
 
 winget install --id Valve.Steam --exact --accept-source-agreements  --accept-source-agreements --force
 
+winget install --id Logitech.GHUB --exact --accept-source-agreements  --accept-source-agreements --force
+
+winget install --id Logitech.LGS  --exact --accept-source-agreements  --accept-source-agreements --force
+
 Stop-Transcript
