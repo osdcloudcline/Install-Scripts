4d39ab8a2ca1cb6af7e899c9b4c9180db52917a1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-11 01:43:21 -0500

    Update Intel.ps1

--- Individual Scripts/CLI-based/Manufacturer-Specific Scripts/Intel/Intel.ps1
+++ Individual Scripts/CLI-based/Manufacturer-Specific Scripts/Intel/Intel.ps1
@@ -10,12 +10,12 @@ Write-Host "Installing $MfrSoftwareGroup1 Software..." -ForegroundColor Cyan
 Write-Host
 Write-Host
 Write-Verbose "Installing: $IntelSoftware1..." -Verbose
-choco.exe install intel-chipset-device-software -y
+choco install intel-chipset-device-software -y
 Write-Verbose "Installing: $IntelSoftware2..." -Verbose
-choco.exe install intel-driver-update-utility -y
+choco install intel-driver-update-utility -y
 Write-Verbose "Installing: $IntelSoftware3..." -Verbose
-choco.exe install intel-dsa -y
+choco install intel-dsa -y
 Write-Verbose "Installing: $IntelSoftware4..." -Verbose
-choco.exe install intel-killer-performance-suite -y
+choco install intel-killer-performance-suite -y
 Write-Verbose "Installing: $IntelSoftware5..." -Verbose
-choco.exe install intel-processor-identification-utility -y
+choco install intel-processor-identification-utility -y
