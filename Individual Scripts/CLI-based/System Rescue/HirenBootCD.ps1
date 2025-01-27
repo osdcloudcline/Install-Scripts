c97af8d67c2817848e68a8e639200975ded1aa4a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-27 06:23:25 -0500

    Create HirenBootCD.ps1

---
+++ Individual Scripts/CLI-based/System Rescue/HirenBootCD.ps1
@@ -0,0 +1,27 @@
+$SystemRescueLog = "C:\Logs\OSDCloud\Install\SystemRescue\HirenBootCD.log"
+
+Start-Transcript -Path $SystemRescueLog
+
+Get-Date
+
+Import-Module -Name OSD -Force
+
+
+##############################
+## Hiren Boot CD PE URL ######
+##############################
+
+$HirenBootCDPEURL = "https://www.hirensbootcd.org/files/HBCD_PE_x64.iso"
+
+##############################
+## Download destination 
+##############################
+
+$DLDestination = "C:\downloads\SystemRescue"
+
+
+Write-Verbose "Processing: Downloading Hiren's BootCD PE x64..."
+
+Save-WebFile -SourceUrl $HirenBootCDPEURL -DestinationDirectory $DLDestination
+
+Stop-Transcript
