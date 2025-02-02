c5325fdf27095f4304f5551b581d6ad8e9a744ac
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-02 01:52:38 -0500

    Update ClientVDI.ps1

--- CLI-based Scripts/ClientVDI.ps1
+++ CLI-based Scripts/ClientVDI.ps1
@@ -15,6 +15,27 @@ Write-Host '                                                                 ' -
 pause
 
 
+
+Write-Verbose "Step 1 - PowerShell Package Providers..." -Verbose
+
+$PSProviders = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Package%20Providers/Install.ps1")
+Invoke-Expression $($PSProviders.Content)
+
+Write-Verbose "Step 2 - PowerShell 5.1 Modules..." -Verbose
+
+$PS5Modules = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%205.1/Install.ps1")
+Invoke-Expression $($PS5Modules.Content)
+
+Write-Verbose "Step 3 - PowerShell 7.x Modules..." -Verbose 
+
+$PS7Modules = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/PowerShell%20Modules/Version%207.x/Install.ps1")
+pwsh Invoke-Expression $($PS7Modules.Content)
+
+Write-Verbose "Step 4 - Merging Registry Entries..." -Verbose
+
+$ClientRegistry = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/Registry%20Modifications/ClientRegistry.ps1")
+Invoke-Expression $($ClientRegistry.Content)
+
 Write-Host "Processing install for: Web Browsers..." -ForegroundColor DarkBlue -BackgroundColor White
 
 $DuckDuckGo = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/main/Individual%20Scripts/CLI-based/Virtual%20Desktop%20Interface/Client%20Operating%20Systems/Browsers/DuckDuckGo.ps1")
