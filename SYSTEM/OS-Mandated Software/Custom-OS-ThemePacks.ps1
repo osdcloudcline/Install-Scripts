ae19d599fe562b3075db50eb6907a72a54a9061a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-02 01:08:29 -0500

    Update Custom-OS-ThemePacks.ps1

--- SYSTEM/OS-Mandated Software/Custom-OS-ThemePacks.ps1
+++ SYSTEM/OS-Mandated Software/Custom-OS-ThemePacks.ps1
@@ -27,8 +27,7 @@ $StarWarsOSThemePack1URL = "https://github.com/osdcloudcline/OS-Items-/raw/refs/
 
 $MCUOSThemePackDownloadDirectory = "C:\downloads\OS\Themes\MCU"
 $StarWarsOSThemePackDownloadDirectory = "C:\downloads\OS\Themes\StarWars"
-$OSThemePackZIPDownloadDirectory = "C:\downloads\OS\Themes\ZIP"
-$ExtractDir = "C:\downloads\OS\Themes\Extract"
+
 
 Write-Verbose "Processing: Acquiring Marvel Cinematic Universe Windows OS Themes" -Verbose
 Write-Host
@@ -52,45 +51,17 @@ Save-WebFile -SourceUrl $StarWarsOSThemePack1URL -DestinationDirectory $StarWars
 Write-Verbose "Completed: Download of Star Wars Windows OS Themes" -Verbose
 Write-Host
 
-Write-Verbose "Processing: Extracting ZIP Files" -Verbose
-Write-Host
-
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\BingMapsEurope.zip.001" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\BingMapsEurope.zip.002" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\BingMapsUnitedStates.zip.001" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\BingMapsUnitedStates.zip.002" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\BingMapsUnitedStates.zip.003" -TargetPath $ExtractDir -ErrorAction SilentlyContinue
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\CommunityShowcaseAqua3.zip.001" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\CommunityShowcaseAqua3.zip.002" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\CommunityShowcaseAqua3.zip.003" -TargetPath $ExtractDir -ErrorAction SilentlyContinue
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\CommunityShowcaseAqua4.zip.001" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\CommunityShowcaseAqua4.zip.002" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\CommunityShowcaseAqua4.zip.003" -TargetPath $ExtractDir -ErrorAction SilentlyContinue
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\PanoramasEurope.zip.001" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$OSThemePackZIPDownloadDirectory\PanoramasEurope.zip.002" -TargetPath $ExtractDir -ErrorAction SilentlyContinue 
-
-Write-Verbose "Completed: Extracting ZIP Files" -Verbose
-Write-Host
-
-Write-Verbose "Processing: Copying Extracted OS Theme Files" -Verbose
-Write-Host
-
-Copy-Item -Path "$ExtractDir\BingMapsEurope.themepack" -Destination $OSThemePackDownloadDirectory -Force
-Copy-Item -Path "$ExtractDir\BingMapsUnitedStates.themepack" -Destination $OSThemePackDownloadDirectory -Force
-Copy-Item -Path "$ExtractDir\CommunityShowcaseAqua3.themepack" -Destination $OSThemePackDownloadDirectory -Force
-Copy-Item -Path "$ExtractDir\CommunityShowcaseAqua4.themepack" -Destination $OSThemePackDownloadDirectory -Force
-Copy-Item -Path "$ExtractDir\PanoramasEurope.deskthemepack" -Destination $OSThemePackDownloadDirectory -Force
-
-Write-Verbose "Completed: Copying Extracted OS Theme Files" -Verbose
-Write-Host
 
 pause
 
 Write-Verbose "Processing: Copying OS Theme Files to C:\Windows\Resources\Themes" -Verbose
 Write-Host
 
-Copy-Item -Path "$OSThemePackDownloadDirectory\*.themepack" -Destination "C:\Windows\Resources\Themes" -Recurse -Force
-Copy-Item -Path "$OSThemePackDownloadDirectory\*.deskthemepack" -Destination "C:\Windows\Resources\Themes" -Recurse -Force
+Copy-Item -Path "$MCUOSThemePackDownloadDirectory\*.themepack" -Destination "C:\Windows\Resources\Themes" -Recurse -Force
+Copy-Item -Path "$MCUOSThemePackDownloadDirectory\*.deskthemepack" -Destination "C:\Windows\Resources\Themes" -Recurse -Force
+
+Copy-Item -Path "$StarWarsOSThemePackDownloadDirectory\*.themepack" -Destination "C:\Windows\Resources\Themes" -Recurse -Force
+Copy-Item -Path "$StarWarsOSThemePackDownloadDirectory\*.deskthemepack" -Destination "C:\Windows\Resources\Themes" -Recurse -Force
 
 Write-Verbose "Completed: Copying OS Theme Files to C:\Windows\Resources\Themes" -Verbose
 Write-Host
