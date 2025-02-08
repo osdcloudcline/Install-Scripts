8587ffefd333535d19e12fca5fb251fd61e9b462
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 13:46:22 -0500

    Update GlaryUtilities.ps1

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/GlaryUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/GlaryUtilities.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Utilities-Software\$env:computername-GlaryUtilities.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Advanced\Utilities-Software\$env:computername-GlaryUtilities.log"
 
 $Date = Get-Date
 
