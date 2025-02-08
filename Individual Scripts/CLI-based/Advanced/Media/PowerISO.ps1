01e3ba251d4cd7a0beecfa0dd31fd6bfae424d36
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 13:36:31 -0500

    Update PowerISO.ps1

--- Individual Scripts/CLI-based/Advanced/Media/PowerISO.ps1
+++ Individual Scripts/CLI-based/Advanced/Media/PowerISO.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Media\$env:computername-PowerISO.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Advanced\Media\$env:computername-PowerISO.log"
 
 $Date = Get-Date
 
