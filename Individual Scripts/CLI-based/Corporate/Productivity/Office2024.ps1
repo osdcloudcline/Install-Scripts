28b24c53d9d7ebcb38faf28ecaa473e509c1ca96
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 13:25:43 -0500

    Update and rename Office2021.ps1 to Office2024.ps1

---
+++ Individual Scripts/CLI-based/Corporate/Productivity/Office2024.ps1
@@ -0,0 +1,17 @@
+Start-Transcript -Path "C:\Logs\Powershell\Install\Corporate\Productivity\$env:computername-Office2024.log"
+
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+$OfficeGHdownloads = "C:\Office\2024" 
+mkdir $OfficeGHdownloads
+$Office2024Files1Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/Configuration-Office2024-EntireSuiteVL.xml"
+$Office2024Files2Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/officedeploymenttool_18227-20162.exe"
+$Office2024Files3Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/setup.exe"
+
+
+Save-WebFile -SourceUrl $Office2024Files1Url -DestinationDirectory $OfficeGHdownloads
+Save-WebFile -SourceUrl $Office2024Files2Url -DestinationDirectory $OfficeGHdownloads
+Save-WebFile -SourceUrl $Office2024Files3Url -DestinationDirectory $OfficeGHdownloads
+
+Stop-Transcript
