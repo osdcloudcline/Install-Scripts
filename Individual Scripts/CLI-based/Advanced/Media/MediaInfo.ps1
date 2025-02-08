5dbf742ce886005b6adf24a1dd208509e01e6b13
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 13:35:57 -0500

    Update MediaInfo.ps1

--- Individual Scripts/CLI-based/Advanced/Media/MediaInfo.ps1
+++ Individual Scripts/CLI-based/Advanced/Media/MediaInfo.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Media\$env:computername-MediaInfo.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Advanced\Media\$env:computername-MediaInfo.log"
 
 $Date = Get-Date
 
