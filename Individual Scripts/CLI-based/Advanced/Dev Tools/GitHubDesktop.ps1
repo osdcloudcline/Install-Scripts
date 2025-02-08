6fdeb79fa8f99b23b3b442e7d48ccf9b633619a3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 13:30:28 -0500

    Update GitHubDesktop.ps1

--- Individual Scripts/CLI-based/Advanced/Dev Tools/GitHubDesktop.ps1
+++ Individual Scripts/CLI-based/Advanced/Dev Tools/GitHubDesktop.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Dev Tools\$env:computername-GitHubDesktop.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Advanced\Dev Tools\$env:computername-GitHubDesktop.log"
 
 $Date = Get-Date
 
