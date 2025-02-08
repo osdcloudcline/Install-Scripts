420a990a500792cbd8117428dcd87ea1739b1a4e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 13:52:42 -0500

    Update WiseRegistryCleaner.ps1

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/WiseRegistryCleaner.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/WiseRegistryCleaner.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Utilities-Software\$env:computername-WRC.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Advanced\Utilities-Software\$env:computername-WRC.log"
 
 $Date = Get-Date
 
