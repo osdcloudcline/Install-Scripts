6cd63be8dc7d3ee7e2c90eec247817d42bf9982d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 18:21:53 -0500

    Update VMWareHorizon.ps1

--- Individual Scripts/CLI-based/Corporate/VDI-Remote Desktop/VMWareHorizon.ps1
+++ Individual Scripts/CLI-based/Corporate/VDI-Remote Desktop/VMWareHorizon.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Corporate\VDI\$env:computername-VMWareHorizon.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Corporate\VDI\$env:computername-VMWareHorizon.log"
 
 $Date = Get-Date
 
