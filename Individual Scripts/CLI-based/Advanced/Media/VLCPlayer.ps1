24efdc1b13a64765a6ea356424bb1e39eaf46fa7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 13:36:49 -0500

    Update VLCPlayer.ps1

--- Individual Scripts/CLI-based/Advanced/Media/VLCPlayer.ps1
+++ Individual Scripts/CLI-based/Advanced/Media/VLCPlayer.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Media\$env:computername-VLCPlayer.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Advanced\Media\$env:computername-VLCPlayer.log"
 
 $Date = Get-Date
 
