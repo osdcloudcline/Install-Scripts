db353fd8cbcc14fb4724a5d53a38eab5ad469a20
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 13:42:30 -0500

    Update CrucialStorage.ps1

--- Individual Scripts/CLI-based/Advanced/Utilities-Hardware/CrucialStorage.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Hardware/CrucialStorage.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Advanced\Utilities-Hardware\$env:computername-CrucialStorage.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Advanced\Utilities-Hardware\$env:computername-CrucialStorage.log"
 
 $Date = Get-Date
 
