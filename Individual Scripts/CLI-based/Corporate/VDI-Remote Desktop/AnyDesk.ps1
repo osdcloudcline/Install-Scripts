e39857957bc535afdea33e4974530f9a6108b319
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 18:21:13 -0500

    Update AnyDesk.ps1

--- Individual Scripts/CLI-based/Corporate/VDI-Remote Desktop/AnyDesk.ps1
+++ Individual Scripts/CLI-based/Corporate/VDI-Remote Desktop/AnyDesk.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Corporate\VDI\$env:computername-AnyDesk.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Corporate\VDI\$env:computername-AnyDesk.log"
 
 $Date = Get-Date
 
