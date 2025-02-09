1cc29ae1e15815009570f0a0927265457e61cae9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:50:43 -0500

    Update Office2021.ps1

--- Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Productivity/Office2021.ps1
+++ Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Productivity/Office2021.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Client VDI\Productivity\$env:computername-Office2021.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Client VDI\Productivity\$env:computername-Office2021.log"
 
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
 Import-Module -Name OSD -Force
