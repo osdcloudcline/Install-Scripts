0c424a040c7981c363ee62bdfded9af2aca60f48
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:45:03 -0500

    Update Win11-22H2-OSKits.ps1

--- Individual Scripts/CLI-based/Standard/Utilities-Software/Win11-22H2-OSKits.ps1
+++ Individual Scripts/CLI-based/Standard/Utilities-Software/Win11-22H2-OSKits.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Utilities-Software\$env:computername-OSADKSDKPE.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Utilities-Software\$env:computername-OSADKSDKPE.log"
 
 $Date = Get-Date
 
