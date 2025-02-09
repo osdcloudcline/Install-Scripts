e61f77c3fb3d5b31327e480f1dd7b2c257b0880e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:38:16 -0500

    Update VSCode.ps1

--- Individual Scripts/CLI-based/Standard/Programming/VSCode.ps1
+++ Individual Scripts/CLI-based/Standard/Programming/VSCode.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Programming\$env:computername-VSCode.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Programming\$env:computername-VSCode.log"
 
 $Date = Get-Date
 
