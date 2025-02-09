be935235e12010e25cf4032f32f54923255be8bb
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:52:07 -0500

    Update 7Zip.ps1

--- Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Utilities-Software/7Zip.ps1
+++ Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Utilities-Software/7Zip.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Client VDI\Utilities-Software\$env:computername-7Zip.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Client VDI\Utilities-Software\$env:computername-7Zip.log"
 
 $Date = Get-Date
 
