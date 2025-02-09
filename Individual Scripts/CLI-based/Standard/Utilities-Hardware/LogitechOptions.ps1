d443fab6b67ce50f6d23952cbf292e410e49ab94
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:40:17 -0500

    Update LogitechOptions.ps1

--- Individual Scripts/CLI-based/Standard/Utilities-Hardware/LogitechOptions.ps1
+++ Individual Scripts/CLI-based/Standard/Utilities-Hardware/LogitechOptions.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Utilities-Hardware\$env:computername-LogitechOpitions.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Utilities-Hardware\$env:computername-LogitechOpitions.log"
 
 $Date = Get-Date
 
