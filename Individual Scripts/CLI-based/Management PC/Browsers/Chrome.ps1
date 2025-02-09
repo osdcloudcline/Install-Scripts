4e249be649b930c179817c080825801e945d3d28
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:21:48 -0500

    Update Chrome.ps1

--- Individual Scripts/CLI-based/Management PC/Browsers/Chrome.ps1
+++ Individual Scripts/CLI-based/Management PC/Browsers/Chrome.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Management PC\Browsers\$env:computername-Chrome.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Management PC\Browsers\$env:computername-Chrome.log"
 
 $Date = Get-Date
 
