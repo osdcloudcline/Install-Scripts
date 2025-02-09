4d3c31389947cd95dd445fae0f84d1feeb5b8047
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:51:02 -0500

    Update AICursorEditor.ps1

--- Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Programming/AICursorEditor.ps1
+++ Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Programming/AICursorEditor.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Client VDI\Programming\$env:computername-AICursorEditor.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Client VDI\Programming\$env:computername-AICursorEditor.log"
 
 $Date = Get-Date
 
