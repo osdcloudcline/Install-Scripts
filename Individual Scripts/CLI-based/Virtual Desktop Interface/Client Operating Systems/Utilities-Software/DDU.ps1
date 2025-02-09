c96cba1023dcba79f722b52859f791c44cc64591
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:52:45 -0500

    Update DDU.ps1

--- Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Utilities-Software/DDU.ps1
+++ Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Utilities-Software/DDU.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Client VDI\Utilities-Software\$env:computername-DDU.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Client VDI\Utilities-Software\$env:computername-DDU.log"
 
 $Date = Get-Date
 
