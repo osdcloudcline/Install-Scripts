f5ff0a1cf584044c953ec17cc3077f3b18368345
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:39:33 -0500

    Update Norton360.ps1

--- Individual Scripts/CLI-based/Standard/Security/Anti-Virus/Norton360.ps1
+++ Individual Scripts/CLI-based/Standard/Security/Anti-Virus/Norton360.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Security\$env:computername-Norton360Download.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Security\$env:computername-Norton360Download.log"
 
 $app1 = "Norton 360"
 
