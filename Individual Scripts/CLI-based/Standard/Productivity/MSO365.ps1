d771aaa9536f6d314a1f7be933203eae91d99b9f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:37:07 -0500

    Update MSO365.ps1

--- Individual Scripts/CLI-based/Standard/Productivity/MSO365.ps1
+++ Individual Scripts/CLI-based/Standard/Productivity/MSO365.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Productivity\$env:computername-MSO365.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Productivity\$env:computername-MSO365.log"
 
 $app1 = "Microsoft 365 (Office)"
 
