cc421956240940e2f65da07804f8a6ad1d8eecc1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:41:51 -0500

    Update SamsungMagician.ps1

--- Individual Scripts/CLI-based/Standard/Utilities-Hardware/SamsungMagician.ps1
+++ Individual Scripts/CLI-based/Standard/Utilities-Hardware/SamsungMagician.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Samsung\Utilities-Hardware\$env:computername-SamsungMagician.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Samsung\Utilities-Hardware\$env:computername-SamsungMagician.log"
 
 $Date = Get-Date
 
