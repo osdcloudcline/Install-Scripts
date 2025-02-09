873f2289ba809f7e76f77b29037730b3e632b369
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:22:36 -0500

    Update FileZilla.ps1

--- Individual Scripts/CLI-based/Management PC/File Transfer/FileZilla.ps1
+++ Individual Scripts/CLI-based/Management PC/File Transfer/FileZilla.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Management PC\File Transfer\$env:computername-FileZilla.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Management PC\File Transfer\$env:computername-FileZilla.log"
 
 $Date = Get-Date
 
