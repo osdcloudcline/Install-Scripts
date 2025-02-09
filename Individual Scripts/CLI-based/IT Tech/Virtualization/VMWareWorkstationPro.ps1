1f27bce47e12711e74bdfd1a5cc55359a1ea2519
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-08 22:31:34 -0500

    Update VMWareWorkstationPro.ps1

--- Individual Scripts/CLI-based/IT Tech/Virtualization/VMWareWorkstationPro.ps1
+++ Individual Scripts/CLI-based/IT Tech/Virtualization/VMWareWorkstationPro.ps1
@@ -6,7 +6,17 @@ Write-Host "Installed on: $Date"
 
 $app1 = "VMWare Workstation Professional"
 
-Write-Host "Installing: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
-choco install vmwareworkstation --params='"/SERIALNUMBER=VV7RA-0YZ0N-M89EP-JFN7C-P62RF"' -y
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+$VMWareWSProDLURL = "https://softwareupdate.vmware.com/cds/vmw-desktop/ws/17.6.2/24409262/windows/core/VMware-workstation-17.6.2-24409262.exe.tar"
+$destination = "C:\downloads"
+
+Write-Host "Downloading: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
+Save-WebFile -SourceUrl $VMWareWSProDLURL -DestinationDirectory $destination
+
+Write-Host 
+Write-Host "Extracting: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Expand-7Zip -ArchiveFileName "C:\downloads\VMware-workstation-17.6.2-24409262.exe.tar" -TargetPath $destination
 
 Stop-Transcript
