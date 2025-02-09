b473b173e387bb0e3d806f90417494af912f9ce8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:22:13 -0500

    Update UPBR.ps1

--- Individual Scripts/CLI-based/Management PC/Cloud Backups/UPBR.ps1
+++ Individual Scripts/CLI-based/Management PC/Cloud Backups/UPBR.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Management PC\Cloud Backups\$env:computername-UPBR.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Management PC\Cloud Backups\$env:computername-UPBR.log"
 
 $Date = Get-Date
 
