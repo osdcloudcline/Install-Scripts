96f7d8eb1bafc9ddc6ab684932dc000539f8323e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:34:24 -0500

    Update iCloud.ps1

--- Individual Scripts/CLI-based/Standard/Cloud Backups/iCloud.ps1
+++ Individual Scripts/CLI-based/Standard/Cloud Backups/iCloud.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Cloud Backups\$env:computername-iCloud.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Cloud Backups\$env:computername-iCloud.log"
 
 $Date = Get-Date
 
