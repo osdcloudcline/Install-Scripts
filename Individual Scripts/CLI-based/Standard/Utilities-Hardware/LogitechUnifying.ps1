09ee6d9badd4fb7307d4282fccc0e595953d00b5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:40:36 -0500

    Update LogitechUnifying.ps1

--- Individual Scripts/CLI-based/Standard/Utilities-Hardware/LogitechUnifying.ps1
+++ Individual Scripts/CLI-based/Standard/Utilities-Hardware/LogitechUnifying.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Utilities-Hardware\$env:computername-LogitechUnifyingSoftware.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Utilities-Hardware\$env:computername-LogitechUnifyingSoftware.log"
 
 $Date = Get-Date
 
