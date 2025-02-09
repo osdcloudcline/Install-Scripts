866c1c28907aec4509de8195e98d785f1114299d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-09 00:36:27 -0500

    Update iTunes.ps1

--- Individual Scripts/CLI-based/Standard/Media/iTunes.ps1
+++ Individual Scripts/CLI-based/Standard/Media/iTunes.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "C:\Logs\Powershell\Install\Standard\Media\$env:computername-iTunes.log"
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Media\$env:computername-iTunes.log"
 
 $Date = Get-Date
 
