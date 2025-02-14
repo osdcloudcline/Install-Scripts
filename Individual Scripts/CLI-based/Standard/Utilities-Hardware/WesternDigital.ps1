f5eb549fe20a42ce0bfaf96208bb4e811cd8f069
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-14 00:22:43 -0500

    Create WesternDigital.ps1

---
+++ Individual Scripts/CLI-based/Standard/Utilities-Hardware/WesternDigital.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Advanced\Utilities-Hardware\$env:computername-WD-Dashboard.log"
+
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+$app1 = "Western Digital Dashboard"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+winget install --id WesternDigital.Dashboard --exact --accept-source-agreements  --accept-package-agreements --force
+
+Stop-Transcript
