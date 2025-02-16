61bd4c38d5c5930fcef12b409369600b85456b90
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-02-16 08:33:06 -0500

    Update Steam.ps1

--- Individual Scripts/CLI-based/Standard/Gaming/Steam.ps1
+++ Individual Scripts/CLI-based/Standard/Gaming/Steam.ps1
@@ -1,13 +1,25 @@
 Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Gaming\$env:computername-Steam.log"
 
+Import-Module -Name OSD -Force
+
 $Date = Get-Date
 
 $app1 = "Steam"
 
+
 Write-Host "Installed on: $Date"
 
 Write-Host
-Write-Host "Installing $app1..." -ForegroundColor Cyan
-winget install --id Valve.Steam --exact --accept-source-agreements  --accept-source-agreements --force
+Write-Verbose "Processing: Steam Setup download..." -Verbose
+$SteamURL = "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe"
+$SteamDestination = "C:\downloads"
+
+Save-WebFile -SourceUrl $SteamURL -DestinationDirectory $SteamDestination
+
+Write-Host
+
+Write-Verbose "Installing: $app1..." -ForegroundColor Cyan
+$SteamSetup = "C:\downloads\SteamSetup.exe"
+Start-Process -FilePath $SteamSetup
 
 Stop-Transcript
