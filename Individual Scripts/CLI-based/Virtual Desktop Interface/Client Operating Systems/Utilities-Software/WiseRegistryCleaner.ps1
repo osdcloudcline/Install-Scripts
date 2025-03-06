f25872f77423c4b999b2d0658395975bd8b9f856
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-05 22:40:17 -0500

    Update WiseRegistryCleaner.ps1

--- Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Utilities-Software/WiseRegistryCleaner.ps1
+++ Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Utilities-Software/WiseRegistryCleaner.ps1
@@ -4,10 +4,16 @@ $Date = Get-Date
 
 Write-Host "Installed on: $Date"
 
+Import-Module -Name OSD -Force
+
 $app1 = "Wise Registry Cleaner"
 
 Write-Host
 Write-Verbose "Installing $app1..." -Verbose
-winget install --id XPDLS1XBTXVPP4 --exact --accept-source-agreements --accept-source-agreements --force
+
+$WiseURL = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/Wise/WRCFree_11.1.10.725.exe"
+$destination = "C:\downloads"
+
+Save-WebFile -SourceUrl $WiseURL -DestinationDirectory $destination
 
 Stop-Transcript
