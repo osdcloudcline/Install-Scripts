3d80e44bb923246660bc4f4a3fb9dba25f211018
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-06 18:40:29 -0500

    Update SmartDefrag.ps1

--- Individual Scripts/CLI-based/IT Tech/Utilities-Software/SmartDefrag.ps1
+++ Individual Scripts/CLI-based/IT Tech/Utilities-Software/SmartDefrag.ps1
@@ -1,1 +1,23 @@
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\IT\Utilities-Software\$env:computername-SmartDefrag.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+Import-Module -Name OSD -Force
+
+$app1 = "Smart Defrag"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+
+$DefragURL = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/Wise/WRCFree_11.1.10.725.exe"
+$destination = "C:\downloads"
+
+Save-WebFile -SourceUrl $DefragURL -DestinationDirectory $destination
+
+$SourceFile = "C:\downloads\WRCFree_11.1.10.725.exe"
+
+Start-Process -FilePath $SourceFFile 
+
+Stop-Transcript
