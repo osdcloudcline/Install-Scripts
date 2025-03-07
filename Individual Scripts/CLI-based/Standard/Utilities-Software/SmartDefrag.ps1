974995cdae7ca70308bc2259c58b76a17f2fc414
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-06 23:37:19 -0500

    Update SmartDefrag.ps1

--- Individual Scripts/CLI-based/Standard/Utilities-Software/SmartDefrag.ps1
+++ Individual Scripts/CLI-based/Standard/Utilities-Software/SmartDefrag.ps1
@@ -1,1 +1,23 @@
+Start-Transcript -Path "C:\Logs\OSDCloud\Install\Standard\Utilities-Software\$env:computername-SmartDefrag.log"
 
+$Date = Get-Date
+
+Write-Host "Installed on: $Date"
+
+Import-Module -Name OSD -Force
+
+$app1 = "Smart Defrag"
+
+Write-Host
+Write-Verbose "Installing $app1..." -Verbose
+
+$DefragURL = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/IoBit/Smart%20Defrag/version%2010.3.0.435/smart-defrag-setup.exe"
+$destination = "C:\downloads"
+
+Save-WebFile -SourceUrl $DefragURL -DestinationDirectory $destination
+
+$SourceFile = "C:\downloads\smart-defrag-setup.exe"
+
+Start-Process -FilePath $SourceFile 
+
+Stop-Transcript
