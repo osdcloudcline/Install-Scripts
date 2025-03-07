7293fdcdabb2040e7bf3f3bf23f6b082f5f31318
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-07 00:46:41 -0500

    Update AICursorEditor.ps1

--- Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Programming/AICursorEditor.ps1
+++ Individual Scripts/CLI-based/Virtual Desktop Interface/Client Operating Systems/Programming/AICursorEditor.ps1
@@ -9,6 +9,6 @@ $app1 = "AI Cursor Editor"
 
 Write-Host
 Write-Host "Installing $app1..." -ForegroundColor Cyan
-winget install --id CursorAI,Inc.Cursor --exact --accept-source-agreements  --accept-source-agreements --force
+winget install --id Anysphere.Cursor --exact --accept-source-agreements  --accept-source-agreements --force
 
 Stop-Transcript
