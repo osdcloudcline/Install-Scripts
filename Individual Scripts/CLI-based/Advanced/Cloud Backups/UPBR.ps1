87fbb4446e7d89c2909aa1649e01f47158a93a8e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-03-08 09:09:49 -0500

    Update UPBR.ps1

--- Individual Scripts/CLI-based/Advanced/Cloud Backups/UPBR.ps1
+++ Individual Scripts/CLI-based/Advanced/Cloud Backups/UPBR.ps1
@@ -17,13 +17,13 @@ Write-Host "Acquiring and Downloading $app5 from OSDCloudCline GitHub Repo..." -
 
 $UPBR = "https://github.com/osdcloudcline/OSDCloud/raw/main/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe"
 $UPBRFilePath = "C:\downloads\UserProfileBackupRestore.exe"
-$UPBRFilePathNewName = "C:\downloads\UPBR.exe"
+$UPBRFilePathNewName = "C:\downloads\User Profile Backup and Restore.exe"
 $UPBRDir = "C:\downloads"
 Save-WebFile -SourceUrl $UPBR -DestinationDirectory $UPBRDir
 Rename-Item -Path $UPBRFilePath  -NewName $UPBRFilePathNewName
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\UPBR.lnk")
-$Shortcut.TargetPath = "C:\downloads\UPBR.exe"
+$Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\User Profile Backup and Restore.lnk")
+$Shortcut.TargetPath = "C:\downloads\User Profile Backup and Restore.exe"
 $Shortcut.Save()
 
 Stop-Transcript
