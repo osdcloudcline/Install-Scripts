4c45193d47f3ae4389cad704d12eb96fbbb9e496
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 09:35:18 -0400

    Update MDT.ps1

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/MDT.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/MDT.ps1
@@ -1,1 +1,13 @@
 $app1 = "Microsoft Deployment Toolkit"
+
+Write-Host
+Write-Verbose "Acquiring $app1 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose 
+$OSDCloudGHdownloads = "C:\OSDCloud\GitHub\downloads"
+$MDTEXE = "C:\OSDCloud\GitHub\downloads\MicrosoftDeploymentToolkit_x64.msi"
+$MDTKUrl = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/MDT/MicrosoftDeploymentToolkit_x64.msi"
+Write-Verbose "Processing and Downloading: $app1 Setup File..." -Verbose
+Save-WebFile -SourceUrl $MDTKUrl -DestinationDirectory $OSDCloudGHdownloads
+
+Start-Process -FilePath "C:\OSDCloud\GitHub\downloads\MicrosoftDeploymentToolkit_x64.msi" -ArgumentList "/quiet /norestart"
+
+pause
