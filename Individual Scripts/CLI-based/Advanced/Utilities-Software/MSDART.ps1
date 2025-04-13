4557eb54367fec4fc7f3d8fbaa5f83fb2a6db351
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 09:38:32 -0400

    Update MSDART.ps1

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/MSDART.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/MSDART.ps1
@@ -1,1 +1,13 @@
+$app1 = "MS DART"
 
+Write-Host
+Write-Verbose "Acquiring $app1 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose 
+$OSDCloudGHdownloads = "C:\OSDCloud\GitHub\downloads"
+$MSDARTEXE = "C:\OSDCloud\GitHub\downloads\MSDaRT100.msi"
+$DARTUrl = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/DaRT/version%2010.0%20x64/MSDaRT100.msi"
+Write-Verbose "Processing and Downloading: $app1 Setup File..." -Verbose
+Save-WebFile -SourceUrl $MDTKUrl -DestinationDirectory $OSDCloudGHdownloads
+
+Start-Process -FilePath "C:\OSDCloud\GitHub\downloads\MSDaRT100.msi" -ArgumentList "/quiet /norestart"
+
+pause
