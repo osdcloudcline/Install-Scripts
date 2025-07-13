776c4625a55a9a65efded2601e326fadd76df5ae
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-07-13 14:23:46 -0400

    Create Honeypot.ps1

---
+++ Provisioning Package Silent Install Scripts/Honeypot.ps1
@@ -0,0 +1,52 @@
+winget install --id Chocolatey.Chocolatey -e --silent --exact --accept-source-agreements --accept-source-agreements --force --source winget
+
+winget install --id Microsoft.DotNet.SDK.8 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.DotNet.AspNetCore.3_1 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.DotNet.AspNetCore.5 --silent --exact --accept-source-agreements --accept-source-agreements --force 
+winget install --id Microsoft.DotNet.AspNetCore.6 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.DotNet.AspNetCore.7 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.DotNet.AspNetCore.8 --silent --exact --accept-source-agreements --accept-source-agreements --force
+
+winget install --id  Microsoft.DotNet.Runtime.3_1 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.DotNet.Runtime.5 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.DotNet.Runtime.6 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.DotNet.Runtime.7 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.DotNet.Runtime.8 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.DotNet.Runtime.9 --silent --exact --accept-source-agreements --accept-source-agreements --force
+
+winget install --id Microsoft.VCRedist.2005.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.VCRedist.2005.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.VCRedist.2008.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.VCRedist.2008.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force 
+winget install --id Microsoft.VCRedist.2010.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force 
+winget install --id Microsoft.VCRedist.2010.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force 
+winget install --id Microsoft.VCRedist.2012.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force 
+winget install --id Microsoft.VCRedist.2012.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force 
+winget install --id Microsoft.VCRedist.2013.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force 
+winget install --id Microsoft.VCRedist.2013.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.VCRedist.2015+.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.VCRedist.2015+.x86  --silent --exact --accept-source-agreements --accept-source-agreements --force
+choco install vcredist2017 -y
+
+winget install --id Oracle.JavaRuntimeEnvironment --silent --exact --accept-source-agreements --accept-source-agreements --force
+
+winget install --id Microsoft.XNARedist --silent --exact --accept-source-agreements --accept-source-agreements --force
+
+choco install openal -y
+
+winget install --id Microsoft.Edge --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id Microsoft.EdgeDriver --silent --exact --accept-source-agreements --accept-source-agreements --force
+
+winget install --id Microsoft.WindowsTerminal --silent --exact --accept-source-agreements --accept-source-agreements --force
+
+winget install --id Google.Chrome --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id DuckDuckGo.DesktopBrowser --silent --exact --accept-source-agreements --accept-source-agreements --force
+
+winget install --id MoritzBunkus.MKVToolNix --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id GuinpinSoft.MakeMKV  --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id MediaArea.MediaInfo.GUI --silent --exact --accept-source-agreements --accept-source-agreements --force
+winget install --id VideoLAN.VLC --silent --exact --accept-source-agreements --accept-source-agreements --force
+
+winget install --id 7zip.7zip --silent --exact --accept-source-agreements --accept-source-agreements --force 
+winget install --id Piriform.CCleaner --silent --exact --accept-source-agreements --accept-source-agreements --force 
+winget install --id Wagnardsoft.DisplayDriverUninstaller --silent --exact --accept-source-agreements --accept-source-agreements --force 
