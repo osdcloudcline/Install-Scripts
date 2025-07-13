1d9bf0a8309c45d17da75dacdce72497dca337ae
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-07-13 15:34:45 -0400

    Update Advanced.ps1

--- Provisioning Package Silent Install Scripts/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/Advanced.ps1
@@ -92,3 +92,31 @@ winget install --id AnyDeskSoftwareGmbH.AnyDesk --silent --exact --accept-source
 choco install vmware-horizon-client -y
 
 
+winget install --id Glarysoft.GlaryUtilities --silent --exact --accept-source-agreements --accept-source-agreements --force 
+$OSDCloudGHdownloads = "C:\downloads"
+$GlaryZipURL1 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/Glary%20Utilities/GlaryUtilities1.zip"
+$GlaryZipURL2 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/Glary%20Utilities/GlaryUtilities2.zip"
+$GlaryUnZipPath = "C:\downloads\GlaryUtilities"
+
+Save-WebFile -SourceUrl $GlaryZipURL1 -DestinationDirectory $OSDCloudGHdownloads
+Save-WebFile -SourceUrl $GlaryZipURL2 -DestinationDirectory $OSDCloudGHdownloads
+Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities1.zip" -TargetPath $GlaryUnZipPath 
+Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities2.zip" -TargetPath $GlaryUnZipPath 
+Copy-Item -Path "C:\downloads\GlaryUtilities" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
+Stop-Process -Name Integrator.exe -Force
+Stop-Process -Name MemfilesService.exe -Force
+Copy-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities\*.*" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
+Remove-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities" -Force -Confirm:$false
+Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Glary Utilities.lnk" -Destination "C:\Users\$env:username\Desktop" -Force
+
+$OfficeGHdownloads = "C:\Office\2024" 
+mkdir $OfficeGHdownloads
+$Office2024Files1Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/Configuration-Office2024-EntireSuiteVL.xml"
+$Office2024Files2Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/officedeploymenttool_18227-20162.exe"
+$Office2024Files3Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/setup.exe"
+
+
+Save-WebFile -SourceUrl $Office2024Files1Url -DestinationDirectory $OfficeGHdownloads
+Save-WebFile -SourceUrl $Office2024Files2Url -DestinationDirectory $OfficeGHdownloads
+Save-WebFile -SourceUrl $Office2024Files3Url -DestinationDirectory $OfficeGHdownloads
+
