10dcfdfc35a78d618b52ca1c0e48d66a18eb188c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-07-15 22:40:04 -0400

    Update Advanced.ps1

--- Provisioning Package Silent Install Scripts/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/Advanced.ps1
@@ -139,3 +139,4 @@ cd $OfficeGHdownloads
 $setup = "C:\Office\2024\setup.exe"
 $arguments = "/configure C:\Office\2024\Configuration-Office2024-EntireSuiteVL.xml"
 
+Start-Process -FilePath $setup -ArgumentList $arguments
