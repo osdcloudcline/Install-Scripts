7abe203209001e5822403196296a909592a90b11
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-09-28 06:36:42 -0400

    Update Advanced.ps1

--- Provisioning Package Silent Install Scripts/PPKG Files/Software Configurations/Advanced/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/PPKG Files/Software Configurations/Advanced/Advanced.ps1
@@ -1,1 +1,7 @@
+Install-Module -Name OSD -Force
+Import-Module -Name OSD -Force
 
+
+$ADVCloudStorageURL = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Cloud%20Storage/Advanced%20Software%20Configuration%20-%20Cloud%20Storage.ppkg"
+$destination = ""
+Write-Verbose "Processing: Acquiring Advanced Software Configuration - Cloud Storage PPKG File" - Verbose
