b3d7fc10708f174965a96e6fc59e8e82e8a94ef4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-09-28 06:20:56 -0400

    Update Advanced.ps1

--- Provisioning Package Silent Install Scripts/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/Advanced.ps1
@@ -1,11 +1,23 @@
 # Define the path to the directory containing your .ppkg files
-$PpkgDirectory = "C:\Provisioning"
+$PpkgDirectory = "C:\Provisioning\Advanced\Software"
+$OSPpkgDirectory = "C:\Provisioning\OS"
 
 # Get all .ppkg files in the specified directory
 $PpkgFiles = Get-ChildItem -Path $PpkgDirectory -Filter "*.ppkg"
+$OSPPKGFiles = Get-ChildItem -Path #OSPpkgDirectory -Filter "*.ppkg"
+
+
 
 Write-Host "Provisioning $env:computername..." -ForegroundColor Cyan
 Write-Host
+
+# Loop through each .ppkg file and install it
+foreach ($OSPpkgFile in $OSPpkgFiles) {
+    Write-Host "Installing $($OSPpkgFile.Name)..."
+    Install-ProvisioningPackage -PackagePath $OSPpkgFile.FullName -QuietInstall -ForceInstall
+    Write-Host "$($OSPpkgFile.Name) installed."
+}
+
 
 # Loop through each .ppkg file and install it
 foreach ($PpkgFile in $PpkgFiles) {
@@ -16,69 +28,6 @@ foreach ($PpkgFile in $PpkgFiles) {
 
 Write-Host "All provisioning packages have been processed."
 
-
-
-winget install --id Chocolatey.Chocolatey --silent --exact --accept-source-agreements --accept-source-agreements --force --source winget
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-winget install --id Microsoft.DotNet.SDK.8 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.DotNet.AspNetCore.3_1 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.DotNet.AspNetCore.5 --silent --exact --accept-source-agreements --accept-source-agreements --force 
-winget install --id Microsoft.DotNet.AspNetCore.6 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.DotNet.AspNetCore.7 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.DotNet.AspNetCore.8 --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-winget install --id  Microsoft.DotNet.Runtime.3_1 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.DotNet.Runtime.5 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.DotNet.Runtime.6 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.DotNet.Runtime.7 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.DotNet.Runtime.8 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.DotNet.Runtime.9 --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-winget install --id Microsoft.VCRedist.2005.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.VCRedist.2005.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.VCRedist.2008.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.VCRedist.2008.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force 
-winget install --id Microsoft.VCRedist.2010.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force 
-winget install --id Microsoft.VCRedist.2010.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force 
-winget install --id Microsoft.VCRedist.2012.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force 
-winget install --id Microsoft.VCRedist.2012.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force 
-winget install --id Microsoft.VCRedist.2013.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force 
-winget install --id Microsoft.VCRedist.2013.x86 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.VCRedist.2015+.x64 --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.VCRedist.2015+.x86  --silent --exact --accept-source-agreements --accept-source-agreements --force
-choco install vcredist2017 -y
-
-winget install --id Oracle.JavaRuntimeEnvironment --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-winget install --id Microsoft.XNARedist --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-choco install openal -y
-
-winget install --id Microsoft.Edge --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.EdgeDriver --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-winget install --id Microsoft.WindowsTerminal --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.PowerShell --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-winget install --id Google.Chrome --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id DuckDuckGo.DesktopBrowser --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Mozilla.Firefox --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-winget install --id Dropbox.Dropbox --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Google.GoogleDrive --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Nextcloud.NextcloudDesktop --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Microsoft.OneDrive --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id Apple.iCloud --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-winget install --id Docker.DockerDesktop --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id GitHub.GitHubDesktop --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-choco install filezilla  -y
-winget install --id PuTTY.PuTTY --silent --exact --accept-source-agreements --accept-source-agreements --force
-winget install --id WinSCP.WinSCP --silent --exact --accept-source-agreements --accept-source-agreements --force
-
-winget install --id Valve.Steam  --silent --exact --accept-source-agreements --accept-source-agreements --force
 
 winget install --id Audacity.Audacity --silent --exact --accept-source-agreements --accept-source-agreements --force
 choco install audacity-ffmpeg -y
@@ -96,7 +45,7 @@ winget install --id Apple.iTunes --silent --exact --accept-source-agreements --a
 winget install --id Adobe.Acrobat.Reader.64-bit --silent --exact --accept-source-agreements --accept-source-agreements --force
 
 winget install --id Anysphere.Cursor --silent --exact --accept-source-agreements --accept-source-agreements --force
-choco install visualstudio2022enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US" -y
+choco install visualstudio2022enterprise --package-parameters "'--allWorkloads --includeRecommended --includeOptional --passive --locale en-US --quiet --norestart'" -y
 winget install --id Microsoft.VisualStudioCode --silent --exact --accept-source-agreements --accept-source-agreements --force
 
 winget install --id Crucial.StorageExecutive --silent --exact --accept-source-agreements --accept-source-agreements --force
