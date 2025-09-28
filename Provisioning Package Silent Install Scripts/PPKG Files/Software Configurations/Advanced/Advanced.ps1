5851c4171b6339ba9b358fb89455d3d324a1be42
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-09-28 06:53:07 -0400

    Update Advanced.ps1

--- Provisioning Package Silent Install Scripts/PPKG Files/Software Configurations/Advanced/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/PPKG Files/Software Configurations/Advanced/Advanced.ps1
@@ -2,7 +2,7 @@ Install-Module -Name OSD -Force
 Import-Module -Name OSD -Force
 
 $ADVCloudStorageURL = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Cloud%20Storage/Advanced%20Software%20Configuration%20-%20Cloud%20Storage.ppkg"
-$ADVDevSoftwareURL = ""
+$ADVDevSoftwareURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Development/Advanced%20Software%20Configuration%20-%20Development%20Software.ppkg"
 $ADVFileTransferURL = ""
 $ADVGamingURL = ""
 $ADVWebBrowsingURL = ""
