9d3e9ac9b70cca43eb8308d19df64e833b63b431
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-03 07:47:32 -0400

    Update Advanced.ps1

--- Provisioning Package Silent Install Scripts/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/Advanced.ps1
@@ -1,10 +1,13 @@
 # Define the path to the directory containing your .ppkg files
 $PpkgDirectory = "C:\Provisioning\Advanced\Software"
 $OSPpkgDirectory = "C:\Provisioning\OS"
+$OSDCloudPpkgDirectory = "C:\Provisioning\OSDCloud"
+
 
 # Get all .ppkg files in the specified directory
 $PpkgFiles = Get-ChildItem -Path $PpkgDirectory -Filter "*.ppkg"
-$OSPPKGFiles = Get-ChildItem -Path #OSPpkgDirectory -Filter "*.ppkg"
+$OSPPKGFiles = Get-ChildItem -Path $OSPpkgDirectory -Filter "*.ppkg"
+$OSDCloudPPKGFiles = Get-ChildItem -Path $OSDCloudPpkgDirectory -Filter "*.ppkg"
 
 $AdvancedPPKG = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Advanced.ps1")
 Invoke-Expression $($AdvancedPPKG.Content)
@@ -16,9 +19,15 @@ Write-Host
 foreach ($OSPpkgFile in $OSPpkgFiles) {
     Write-Host "Installing $($OSPpkgFile.Name)..."
     Install-ProvisioningPackage -PackagePath $OSPpkgFile.FullName -QuietInstall -ForceInstall
-    Write-Host "$($OSPpkgFile.Name) installed."
+    Write-Host "$($OSDCloudPpkgFile.Name) installed."
 }
 
+# Loop through each .ppkg file and install it
+foreach ($OSDCloudPpkgFile in $OSDCloudPpkgFiles) {
+    Write-Host "Installing $($PpkgFile.Name)..."
+    Install-ProvisioningPackage -PackagePath $PpkgFile.FullName -QuietInstall -ForceInstall
+    Write-Host "$($PpkgFile.Name) installed."
+}
 
 # Loop through each .ppkg file and install it
 foreach ($PpkgFile in $PpkgFiles) {
