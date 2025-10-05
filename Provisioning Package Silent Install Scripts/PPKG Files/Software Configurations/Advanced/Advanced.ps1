4bcbe64329216a0e0d5653ff0e8b908ac90a57c6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-05 01:59:26 -0400

    Update Advanced.ps1

--- Provisioning Package Silent Install Scripts/PPKG Files/Software Configurations/Advanced/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/PPKG Files/Software Configurations/Advanced/Advanced.ps1
@@ -1,6 +1,8 @@
 Install-Module -Name OSD -Force
 Import-Module -Name OSD -Force
 
+$PS5Modules = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/PowerShell%20Modules/Install/Install%20Client%20OS%20SYSTEM%20PowerShell%20Modules.ppkg"
+$PS7Modules = ""
 $ADVCloudStorageURL = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Cloud%20Storage/Advanced%20Software%20Configuration%20-%20Cloud%20Storage.ppkg"
 $ADVDevSoftwareURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Development/Advanced%20Software%20Configuration%20-%20Development%20Software.ppkg"
 $ADVFileTransferURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/File%20Transfer/Advanced%20Software%20Configuration%20-%20File%20Transfer.ppkg"
@@ -18,9 +20,18 @@ $OSDCloud2 = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/ma
 $OSSystemSoftwareURL = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Client%20OS%20SYSTEM%20Software/Client%20OS%20SYSTEM%20SOFTWARE.ppkg"
 
 $destination = "C:\Provisioning\Advanced\Software"
+$PS5destination = "C:\Provisioning\PS5-Modules"
+$PS7destination = "C:\Provisioning\PS7-Modules"
 $OSdestination = "C:\Provisioning\OS"
 $OSDClouddestination = "C:\Provisioning\OSDCloud"
 
+
+Write-Verbose "Processing: Acquiring Client Operating System SYSTEM PowerShell 5.1 Package Providers and Modules PPKG File" - Verbose
+Save-WebFile -SourceUrl $PS5Modules -DestrinationDirectory $PS5destination
+Write-Host
+Write-Verbose "Processing: Acquiring Client Operating System SYSTEM PowerShell 7.x Modules PPKG File" - Verbose
+Save-WebFile -SourceUrl $PS7Modules  -DestrinationDirectory $PS7destination
+Write-Host
 Write-Verbose "Processing: Acquiring Client Operating System SYSTEM Software Configuration PPKG File" - Verbose
 Save-WebFile -SourceUrl $OSSystemSoftwareURL -DestrinationDirectory $OSdestination
 Write-Host
