cddfea9ccdfad6b977e7447b63d62465efeb6bf5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-08 21:57:09 -0400

    Update Advanced.ps1

--- Provisioning Package Silent Install Scripts/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/Advanced.ps1
@@ -46,14 +46,6 @@ choco install vmware-horizon-client -y
 
 
 
-$OfficeGHdownloads = "C:\Office\2024" 
-mkdir $OfficeGHdownloads
-$Office2024Files1Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/Configuration-Office2024-EntireSuiteVL.xml"
-$Office2024Files2Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/officedeploymenttool_18227-20162.exe"
-$Office2024Files3Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/setup.exe"
 
-Save-WebFile -SourceUrl $Office2024Files1Url -DestinationDirectory $OfficeGHdownloads
-Save-WebFile -SourceUrl $Office2024Files2Url -DestinationDirectory $OfficeGHdownloads
-Save-WebFile -SourceUrl $Office2024Files3Url -DestinationDirectory $OfficeGHdownloads
 
 
