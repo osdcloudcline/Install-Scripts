d87821533bcd54966f80074d27e50dfe619cf6f3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-10 08:44:27 -0400

    Update Advanced.ps1

--- Provisioning Package Silent Install Scripts/PPKG Files/Software Configurations/Advanced/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/PPKG Files/Software Configurations/Advanced/Advanced.ps1
@@ -3,21 +3,21 @@ Import-Module -Name OSD -Force
 
 $PS5Modules = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/PowerShell%20Modules/Install/Install%20Client%20OS%20SYSTEM%20PowerShell%20Modules.ppkg"
 $PS7Modules = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/PowerShell%20Modules/Install/Install%20Client%20OS%20SYSTEM%20PowerShell%207.x%20Modules.ppkg"
-$ADVCloudStorageURL = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Cloud%20Storage/Advanced%20Software%20Configuration%20-%20Cloud%20Storage.ppkg"
-$ADVDevSoftwareURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Development/Advanced%20Software%20Configuration%20-%20Development%20Software.ppkg"
-$ADVFileTransferURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/File%20Transfer/Advanced%20Software%20Configuration%20-%20File%20Transfer.ppkg"
-$ADVGamingURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Gaming/Advanced%20Software%20Configuration%20-%20PC%20Gaming.ppkg"
-$ADVWebBrowsingURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Web%20Browsing/Advanced%20Software%20Configuration%20-%20Web%20Browsers.ppkg"
-$HWSoftwareURL = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/System%20Hardware-related%20Software/Advanced%20Software%20Configuration%20-%20System%20Hardware%20Utilities.ppkg"
-$ADVMediaURL = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Media/Advanced%20Software%20Configuration%20-%20Media%20Software.ppkg"
-$ADVProgrammingURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Programming/Advanced%20Software%20Configuration%20-%20Programming.ppkg"
-$ADVProductivityURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Productivity/Advanced%20Software%20Installation%20-%20Productivity%20Software.ppkg"
-$ADVUtilitiesSoftwareURL = ""
-$ADVSecurityURL = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Security/Advanced%20Software%20Configuration%20-%20Security%20Software.ppkg"
-$PCBenchmarkingURL = ""
+$ADVCloudStorage = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Cloud%20Storage/Advanced%20Software%20Configuration%20-%20Cloud%20Storage.ppkg"
+$ADVDevSoftware = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Development/Advanced%20Software%20Configuration%20-%20Development%20Software.ppkg"
+$ADVFileTransfer = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/File%20Transfer/Advanced%20Software%20Configuration%20-%20File%20Transfer.ppkg"
+$ADVGaming = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Gaming/Advanced%20Software%20Configuration%20-%20PC%20Gaming.ppkg"
+$ADVWebBrowsing = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Web%20Browsing/Advanced%20Software%20Configuration%20-%20Web%20Browsers.ppkg"
+$HWSoftware = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/System%20Hardware-related%20Software/Advanced%20Software%20Configuration%20-%20System%20Hardware%20Utilities.ppkg"
+$ADVMedia = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Media/Advanced%20Software%20Configuration%20-%20Media%20Software.ppkg"
+$ADVProgramming = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Programming/Advanced%20Software%20Configuration%20-%20Programming.ppkg"
+$ADVProductivity = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Productivity/Advanced%20Software%20Installation%20-%20Productivity%20Software.ppkg"
+$ADVUtilitiesSoftware = ""
+$ADVSecurity = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Security/Advanced%20Software%20Configuration%20-%20Security%20Software.ppkg"
+$PCBenchmarking = ""
 $OSDCloud2 = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/OSDCloud%20v2%20Pre-Requisites/OSDCloud%20Version%202.0.ppkg"
 
-$OSSystemSoftwareURL = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Client%20OS%20SYSTEM%20Software/Client%20OS%20SYSTEM%20SOFTWARE.ppkg"
+$OSSystemSoftware = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Client%20OS%20SYSTEM%20Software/Client%20OS%20SYSTEM%20SOFTWARE.ppkg"
 
 $destination = "C:\Provisioning\Advanced\Software"
 $PS5destination = "C:\Provisioning\PS5-Modules"
@@ -26,7 +26,7 @@ $OSdestination = "C:\Provisioning\OS"
 $OSDClouddestination = "C:\Provisioning\OSDCloud"
 
 Write-Verbose "Processing: Acquiring Client Operating System SYSTEM Software Configuration PPKG File" - Verbose
-Save-WebFile -SourceUrl $OSSystemSoftwareURL -DestrinationDirectory $OSdestination
+Save-WebFile -SourceUrl $OSSystemSoftware -DestrinationDirectory $OSdestination
 Write-Host
 Write-Verbose "Processing: Acquiring Client Operating System SYSTEM PowerShell 5.1 Package Providers and Modules PPKG File" - Verbose
 Save-WebFile -SourceUrl $PS5Modules -DestrinationDirectory $PS5destination
@@ -38,37 +38,37 @@ Write-Verbose "Processing: Acquiring OSDCloud Version 2.0 PPKG File" - Verbose
 Save-WebFile -SourceUrl $OSDCloud2 -DestrinationDirectory $OSDClouddestination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Cloud Storage PPKG File" - Verbose
-Save-WebFile -SourceUrl $ADVCloudStorageURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $ADVCloudStorage -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Development Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $ADVDevSoftwareURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $ADVDevSoftware -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - File Transfer Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $ADVFileTransferURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $ADVFileTransfer -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Hardware relateds Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $HWSoftwareURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $HWSoftware -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Gaming Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $ADVGamingURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $ADVGaming -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Media Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $ADVMediaURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $ADVMedia -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Programming Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $ADVProgrammingURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $ADVProgramming -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Productivity Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $ADVProductivityURL  -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $ADVProductivity  -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Utility Software PPKG File" - Verbose
 Save-WebFile -SourceUrl $ADVProgrammingURL -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - PC Benchmarking Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $PCBenchmarkingURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $PCBenchmarking -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Web Browsing Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $ADVWebBrowsingURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $ADVWebBrowsing -DestrinationDirectory $destination
 Write-Host
 Write-Verbose "Processing: Acquiring Advanced Software Configuration - Security Software PPKG File" - Verbose
-Save-WebFile -SourceUrl $ADVSecurityURL -DestrinationDirectory $destination
+Save-WebFile -SourceUrl $ADVSecurity -DestrinationDirectory $destination
