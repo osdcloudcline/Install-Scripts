a63b94702f98204415369a2cf250a9f48d89f5f3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-17 01:24:34 -0400

    Update SYSTEMSoftware.ps1

--- Provisioning Package Silent Install Scripts/PPKG Files/Client OS SYSTEM Software/SYSTEMSoftware.ps1
+++ Provisioning Package Silent Install Scripts/PPKG Files/Client OS SYSTEM Software/SYSTEMSoftware.ps1
@@ -10,3 +10,13 @@ $PS5Modules = "C:\Provisioning\PS5-Modules"
 $PS7Modules = "C:\Provisioning\PS7-Modules"
 
 
+# Get all .ppkg files in the specified directory
+
+$PS5PPKGFiles = Get-ChildItem -Path $PS5Modules -Filter "*.ppkg"
+$PS7PPKGFiles = Get-ChildItem -Path $PS7Modules -Filter "*.ppkg"
+$SYSTEMSoftwarePPKGFiles = Get-ChildItem -Path $SYSTEMSoftwarePpkgDirectory -Filter "*.ppkg"
+$NETPPKGFiles = Get-ChildItem -Path $NETPpkgDirectory -Filter "*.ppkg"
+$OracleJREFiles = Get-ChildItem -Path $OracleJREPpkgDirectory -Filter "*.ppkg"
+$ASPFiles = Get-ChildItem -Path $ASPPpkgDirectory -Filter "*.ppkg"
+
+
