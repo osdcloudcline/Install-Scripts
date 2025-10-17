b239f597ae1c31f2cb40ea1211114854d34e3965
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-17 01:27:31 -0400

    Add loops for installing multiple .ppkg files

--- Provisioning Package Silent Install Scripts/PPKG Files/Client OS SYSTEM Software/SYSTEMSoftware.ps1
+++ Provisioning Package Silent Install Scripts/PPKG Files/Client OS SYSTEM Software/SYSTEMSoftware.ps1
@@ -20,3 +20,57 @@ $OracleJREFiles = Get-ChildItem -Path $OracleJREPpkgDirectory -Filter "*.ppkg"
 $ASPFiles = Get-ChildItem -Path $ASPPpkgDirectory -Filter "*.ppkg"
 $OSDCloudPPKGFiles = Get-ChildItem -Path $OSDCloudPpkgDirectory -Filter "*.ppkg"
 
+# Loop through each .ppkg file and install it
+
+foreach ($PS5PPKGFile in $PS5PPKGFiles) {
+    Write-Host "Installing $($PS5PPKGFile.Name)..."
+    Install-ProvisioningPackage -PackagePath "C:\Provisioning\PS5-Modules\PS5Modules.ppkg" -QuietInstall -ForceInstall
+    Write-Host "$($PS5PPKGFile.Name) installed."
+}
+
+# Loop through each .ppkg file and install it
+
+foreach ($PS7PPKGFile in $PS7PPKGFiles) {
+    Write-Host "Installing $($PS7PPKGFile.Name)..."
+    Install-ProvisioningPackage -PackagePath "C:\Provisioning\PS7-Modules\PS7Modules.ppkg" -QuietInstall -ForceInstall
+    Write-Host "$($PS7PPKGFile.Name) installed."
+}
+
+# Loop through each .ppkg file and install it
+
+foreach ($SYSTEMSoftwarePPKGFile in $SYSTEMSoftwarePPKGFiles) {
+    Write-Host "Installing $($SYSTEMSoftwarePPKGFile.Name)..."
+    Install-ProvisioningPackage -PackagePath "C:\Provisioning\OS\SYSTEM\SYSTEMSoftware.ppkg" -QuietInstall -ForceInstall
+    Write-Host "$($SYSTEMSoftwarePPKGFile.Name) installed."
+}
+
+# Loop through each .ppkg file and install it
+
+foreach ($NETPPKGFile in $NETPPKGFiles) {
+    Write-Host "Installing $($NETPPKGFile.Name)..."
+    Install-ProvisioningPackage -PackagePath "C:\Provisioning\OS\NET\NETFramework.ppkg" -QuietInstall -ForceInstall
+    Write-Host "$($NETPPKGFile.Name) installed."
+}
+
+# Loop through each .ppkg file and install it
+
+foreach ($OracleJREPPKGFile in $OracleJREPPKGFiles) {
+    Write-Host "Installing $($OracleJREPPKGFile.Name)..."
+    Install-ProvisioningPackage -PackagePath "C:\Provisioning\OS\OracleJRE\OracleJRE.ppkg" -QuietInstall -ForceInstall
+    Write-Host "$($OracleJREPPKGFile.Name) installed."
+}
+
+# Loop through each .ppkg file and install it
+
+foreach ($ASPPPKGFile in $ASPPPKGFiles) {
+    Write-Host "Installing $($ASPPPKGFile.Name)..."
+    Install-ProvisioningPackage -PackagePath "C:\Provisioning\OS\OracleJRE\ASPRedistributables.ppkg" -QuietInstall -ForceInstall
+    Write-Host "$($ASPPPKGFile.Name) installed."
+}
+
+# Loop through each .ppkg file and install it
+foreach ($OSDCloudPpkgFile in $OSDCloudPPKGFiles) {
+    Write-Host "Installing $($OSDCloudPpkgFile.Name)..."
+    Install-ProvisioningPackage -PackagePath $OSDCloudPpkgFile.FullName -QuietInstall -ForceInstall
+    Write-Host "$($OSDCloudPpkgFile.Name) installed."
+}
