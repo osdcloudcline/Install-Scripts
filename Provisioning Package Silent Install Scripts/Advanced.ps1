d9376294efa35f14d84901058d7e53d814165297
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-17 01:42:49 -0400

    Enhance script with OS info and PPKG updates
    Added OS version retrieval and updated PPKG file handling.

--- Provisioning Package Silent Install Scripts/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/Advanced.ps1
@@ -1,24 +1,20 @@
 # Define the path to the directory containing your .ppkg files
 $SoftwarePpkgDirectory = "C:\Provisioning\Advanced\Software"
 
-$ASPPpkgDirectory = "C:\Provisioning\OS\ASP"
-$PSProvidersPpkgDirectory = "C:\Provisioning\OS\Package Providers"
-$NETPpkgDirectory = "C:\Provisioning\OS\NET"
-$OracleJREPpkgDirectory = "C:\Provisioning\OS\OracleJRE"
-$SYSTEMSoftwarePpkgDirectory = "C:\Provisioning\OS\SYSTEM"
-$OSDCloudPpkgDirectory = "C:\Provisioning\OSDCloud"
-$PS5Modules = "C:\Provisioning\PS5-Modules"
-$PS7Modules = "C:\Provisioning\PS7-Modules"
-
-# Get all .ppkg files in the specified directory
-$PS5PPKGFiles = Get-ChildItem -Path $PS5Modules -Filter "*.ppkg"
-$PS7PPKGFiles = Get-ChildItem -Path $PS7Modules -Filter "*.ppkg"
-$SYSTEMSoftwarePPKGFiles = Get-ChildItem -Path $SYSTEMSoftwarePpkgDirectory -Filter "*.ppkg"
-$NETPPKGFiles = Get-ChildItem -Path $NETPpkgDirectory -Filter "*.ppkg"
-$OracleJREFiles = Get-ChildItem -Path $OracleJREPpkgDirectory -Filter "*.ppkg"
-$ASPFiles = Get-ChildItem -Path $ASPPpkgDirectory -Filter "*.ppkg"
 $ADVSoftwarePpkgFiles = Get-ChildItem -Path $SoftwarePpkgDirectory -Filter "*.ppkg"
-$OSDCloudPPKGFiles = Get-ChildItem -Path $OSDCloudPpkgDirectory -Filter "*.ppkg"
+
+# Retrieve OS Edition
+
+$OSVerison = $($os.Version)
+$UBR = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name UBR).UBR
+$FullOS = $($os.Caption)
+$OSBuild = $($os.Version) + "." + $UBR
+$DisplayVersion = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name DisplayVersion).DisplayVersion
+$EditionID = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name EditionID).EditionID
+$CompleteOSInfo = "$FullOS" + " $OSBuild" + " $DisplayVersion" + " $EditionID"
+
+Write-Host "Processing:Acquiring PPKG Files for Advanced Software Configuration..." -ForegroundColor Cyan
+Write-Host
 
 $AdvancedPPKG = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Advanced.ps1")
 Invoke-Expression $($AdvancedPPKG.Content)
@@ -35,7 +31,7 @@ Invoke-Expression $($ClientOSRegistry.Content)
 $OSDCloud2 = Invoke-WebRequest("")
 Invoke-Expression $($OSDCloud2.Content)
 
-$ADVSoftware = Invoke-WebReuest("")
+$ADVSoftware = Invoke-WebReuest("https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/ADV-PPKGInstall.ps1")
 Invoke-Expression $$ADVSoftware.Content)
 
 # Loop through each .ppkg file and install it
@@ -45,7 +41,8 @@ foreach ($PpkgFile in $PpkgFiles) {
     Write-Host "$($PpkgFile.Name) installed."
 }
 
-
+Write-Host "Processing:Scanning $env:computername for updates - $CompleteOSInfor ..." -ForegroundColor Cyan
+Write-Host
 
 Import-Module -Name PSWindowsUpdate -Force
 Install-WindowsUpdate -AcceptAll
