4655d14aa7febdb47796e8a89ad15971aacc2372
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-17 01:29:18 -0400

    Refactor Advanced.ps1 to simplify package installation
    Removed multiple foreach loops for installing .ppkg files.

--- Provisioning Package Silent Install Scripts/Advanced.ps1
+++ Provisioning Package Silent Install Scripts/Advanced.ps1
@@ -20,67 +20,14 @@ $ASPFiles = Get-ChildItem -Path $ASPPpkgDirectory -Filter "*.ppkg"
 $ADVSoftwarePpkgFiles = Get-ChildItem -Path $SoftwarePpkgDirectory -Filter "*.ppkg"
 $OSDCloudPPKGFiles = Get-ChildItem -Path $OSDCloudPpkgDirectory -Filter "*.ppkg"
 
-
 $AdvancedPPKG = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Advanced.ps1")
 Invoke-Expression $($AdvancedPPKG.Content)
 
 Write-Host "Provisioning:$env:computername..." -ForegroundColor Cyan
 Write-Host
 
-# Loop through each .ppkg file and install it
-
-foreach ($PS5PPKGFile in $PS5PPKGFiles) {
-    Write-Host "Installing $($PS5PPKGFile.Name)..."
-    Install-ProvisioningPackage -PackagePath "C:\Provisioning\PS5-Modules\PS5Modules.ppkg" -QuietInstall -ForceInstall
-    Write-Host "$($PS5PPKGFile.Name) installed."
-}
-
-# Loop through each .ppkg file and install it
-
-foreach ($PS7PPKGFile in $PS7PPKGFiles) {
-    Write-Host "Installing $($PS7PPKGFile.Name)..."
-    Install-ProvisioningPackage -PackagePath "C:\Provisioning\PS7-Modules\PS7Modules.ppkg" -QuietInstall -ForceInstall
-    Write-Host "$($PS7PPKGFile.Name) installed."
-}
-
-# Loop through each .ppkg file and install it
-
-foreach ($SYSTEMSoftwarePPKGFile in $SYSTEMSoftwarePPKGFiles) {
-    Write-Host "Installing $($SYSTEMSoftwarePPKGFile.Name)..."
-    Install-ProvisioningPackage -PackagePath "C:\Provisioning\OS\SYSTEM\SYSTEMSoftware.ppkg" -QuietInstall -ForceInstall
-    Write-Host "$($SYSTEMSoftwarePPKGFile.Name) installed."
-}
-
-# Loop through each .ppkg file and install it
-
-foreach ($NETPPKGFile in $NETPPKGFiles) {
-    Write-Host "Installing $($NETPPKGFile.Name)..."
-    Install-ProvisioningPackage -PackagePath "C:\Provisioning\OS\NET\NETFramework.ppkg" -QuietInstall -ForceInstall
-    Write-Host "$($NETPPKGFile.Name) installed."
-}
-
-# Loop through each .ppkg file and install it
-
-foreach ($OracleJREPPKGFile in $OracleJREPPKGFiles) {
-    Write-Host "Installing $($OracleJREPPKGFile.Name)..."
-    Install-ProvisioningPackage -PackagePath "C:\Provisioning\OS\OracleJRE\OracleJRE.ppkg" -QuietInstall -ForceInstall
-    Write-Host "$($OracleJREPPKGFile.Name) installed."
-}
-
-# Loop through each .ppkg file and install it
-
-foreach ($ASPPPKGFile in $ASPPPKGFiles) {
-    Write-Host "Installing $($ASPPPKGFile.Name)..."
-    Install-ProvisioningPackage -PackagePath "C:\Provisioning\OS\OracleJRE\ASPRedistributables.ppkg" -QuietInstall -ForceInstall
-    Write-Host "$($ASPPPKGFile.Name) installed."
-}
-
-# Loop through each .ppkg file and install it
-foreach ($OSDCloudPpkgFile in $OSDCloudPPKGFiles) {
-    Write-Host "Installing $($OSDCloudPpkgFile.Name)..."
-    Install-ProvisioningPackage -PackagePath $OSDCloudPpkgFile.FullName -QuietInstall -ForceInstall
-    Write-Host "$($OSDCloudPpkgFile.Name) installed."
-}
+$OS = Invoke-WebRequest("")
+Invoke-Expression $($OS.Content)
 
 # Loop through each .ppkg file and install it
 foreach ($PpkgFile in $PpkgFiles) {
