1482eecb2a1d34203846e2c27f6cd005a4a65f42
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-17 01:23:57 -0400

    Define paths for provisioning package directories
    Added directory paths for various provisioning packages.

--- Provisioning Package Silent Install Scripts/PPKG Files/Client OS SYSTEM Software/SYSTEMSoftware.ps1
+++ Provisioning Package Silent Install Scripts/PPKG Files/Client OS SYSTEM Software/SYSTEMSoftware.ps1
@@ -1,3 +1,12 @@
 # Define the path to the directory containing your .ppkg files
 
+$ASPPpkgDirectory = "C:\Provisioning\OS\ASP"
+$PSProvidersPpkgDirectory = "C:\Provisioning\OS\Package Providers"
+$NETPpkgDirectory = "C:\Provisioning\OS\NET"
+$OracleJREPpkgDirectory = "C:\Provisioning\OS\OracleJRE"
+$SYSTEMSoftwarePpkgDirectory = "C:\Provisioning\OS\SYSTEM"
+$OSDCloudPpkgDirectory = "C:\Provisioning\OSDCloud"
+$PS5Modules = "C:\Provisioning\PS5-Modules"
+$PS7Modules = "C:\Provisioning\PS7-Modules"
+
 
