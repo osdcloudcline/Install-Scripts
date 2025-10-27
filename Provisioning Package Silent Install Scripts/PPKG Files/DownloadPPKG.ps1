540e5f8e0218ab002e9d5af655e2f85e55beb65d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-10-27 00:02:54 -0400

    Update OSASPRedistributables URL in DownloadPPKG.ps1

--- Provisioning Package Silent Install Scripts/PPKG Files/DownloadPPKG.ps1
+++ Provisioning Package Silent Install Scripts/PPKG Files/DownloadPPKG.ps1
@@ -131,7 +131,7 @@ do
         $OSSystemSoftware = "https://github.com/osdcloudcline/Install-Scripts/blob/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Client%20OS%20SYSTEM%20Software/Client%20OS%20SYSTEM%20SOFTWARE.ppkg"
         $ADVRDCVDI = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Software%20Configurations/Advanced/Remote%20Desktop%20Connections%20and%20VDI/Advanced%20Software%20Configuration%20-%20Remote%20Desktop,%20Help%20Desk%20Support%20and%20VDI.ppkg"
         $ClientOSRegistry = ""
-        $OSASPRedistributables = ""
+        $OSASPRedistributables = "https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Provisioning%20Package%20Silent%20Install%20Scripts/PPKG%20Files/Client%20OS%20SYSTEM%20Software/ASPRedistributables.ppkg"
         $NETFramework = ""
         $OracleJRE = ""
         $PackageProviders = ""
