a8d0158e6a9b27ca781052730bec581bbc25bf05
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-01-19 09:34:13 -0500

    Remove SilentlyContinue from Expand-7Zip commands

--- Individual Scripts/CLI-based/Advanced/VDI-RemoteDesktop/MSRemoteDesktop.ps1
+++ Individual Scripts/CLI-based/Advanced/VDI-RemoteDesktop/MSRemoteDesktop.ps1
@@ -20,8 +20,8 @@ $RDPDestination = "C:\downloads\Microsoft Remote Desktop"
 Save-WebFile -SourceUrl $RDP1URL -DestinationDirectory $RDPDestination
 Save-WebFile -SourceUrl $RDP2URL -DestinationDirectory $RDPDestination
 
-Expand-7Zip -ArchiveFileName "$RDPDestination\RemoteDesktop.zip.001" -TargetPath $RDPDestination  -ErrorAction SilentlyContinue 
-Expand-7Zip -ArchiveFileName "$RDPDestination\RemoteDesktop.zip.002" -TargetPath $RDPDestination  -ErrorAction SilentlyContinue 
+Expand-7Zip -ArchiveFileName "$RDPDestination\RemoteDesktop.zip.001" -TargetPath $RDPDestination  
+Expand-7Zip -ArchiveFileName "$RDPDestination\RemoteDesktop.zip.002" -TargetPath $RDPDestination  
 
 Rename-Item -Path "$RDPDestination\RemoteDesktop_1.2.5910.0_x64.msi" -NewName "$RDPDestination\RemoteDesktop.msi" -Force
 
