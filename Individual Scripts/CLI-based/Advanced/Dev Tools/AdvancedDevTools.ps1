e6e3ae38593776c51ad2924ad3ae27e9496129c2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-17 00:02:54 -0400

    Add AdvancedDevTools.ps1 for automated tool installation
    Define a list of advanced development tools and automate their installation using WinGet.

---
+++ Individual Scripts/CLI-based/Advanced/Dev Tools/AdvancedDevTools.ps1
@@ -0,0 +1,16 @@
+# Define the Advanced Dev Tools Software using Hash Tables @{ }
+$ADVDevTools = @(
+   @{ Name = "Docker Desktop"; ID = "Docker.DockerDesktop" }
+   @{ Name = "Git for Windows"; ID = "Git.Git" } 
+   @{ Name = "GitHub Desktop"; ID = "GitHub.GitHubDesktop" }
+)
+
+
+
+# Process WinGet Items
+foreach($App in $ADVDevTools){
+    Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
+    # Added --silent and --accept-package-agreements for better automation
+    winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
+    Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
