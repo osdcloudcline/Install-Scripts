080750b6ec700c2a340bb08b6d33295d710da93c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-16 23:47:26 -0400

    Change loop variable for cloud backup processing

--- Individual Scripts/CLI-based/Advanced/Cloud Backups/AdvancedCloudBackups.ps1
+++ Individual Scripts/CLI-based/Advanced/Cloud Backups/AdvancedCloudBackups.ps1
@@ -10,7 +10,7 @@ $ADVCloudBackups = @(
 
 
 # Process WinGet Items
-foreach($App in $ADVBrowsers){
+foreach($App in $ADVCloudBackups){
     Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
     # Added --silent and --accept-package-agreements for better automation
     winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
