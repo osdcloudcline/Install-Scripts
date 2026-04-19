e0eb789210d3e693cf420568795f4f0ca42ed8d0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-19 12:14:31 -0400

    Add Hyper-V and Windows Sandbox feature enabling

--- Individual Scripts/CLI-based/Advanced/Virtualization/AdvancedVirtualization.ps1
+++ Individual Scripts/CLI-based/Advanced/Virtualization/AdvancedVirtualization.ps1
@@ -1,1 +1,6 @@
-
+Write-Host "Processing Windows Feature: Hyper-V" -ForegroundColor Cyan
+Write-Host
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Write-Host "Processing Windows Feature: Windows Sandbox" -ForegroundColor Cyan
+Write-Host
+Enable-WindowsOptionalFeature -Online -FeatureName "Containers-DisposableClientVM" -All
