340e3f1b90a07e900a1c6ddae262e3e10125c782
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-19 00:47:21 -0400

    Include VMWare Horizon Client in installation script
    Added support for installing VMWare-Omnissa Horizon Client via Chocolatey.

--- Individual Scripts/CLI-based/Advanced/VDI-RemoteDesktop/Advanced-VDIRemoteDesktop.ps1
+++ Individual Scripts/CLI-based/Advanced/VDI-RemoteDesktop/Advanced-VDIRemoteDesktop.ps1
@@ -2,14 +2,25 @@
 $ADVVDIRemote = @(
    @{ Name = "AnyDesk"; ID = "AnyDesk.AnyDesk" }
    @{ Name = "Microsoft Remote Desktop"; ID = "Microsoft.RemoteDesktopClient" }
-   @{ Name = "Microsoft Visual Studio 2026 Enterprise"; ID = "Microsoft.VisualStudio.Enterprise" }
+   
 )
 
+$ChocolateyVDIRemote = @(
+   @{ Name = "VMWare-Omnissa Horizon Client"; ID = "omnissa-horizon-client" }
+)
 
 # Process WinGet Items
 foreach($App in $ADVVDIRemote){
     Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
     # Added --silent and --accept-package-agreements for better automation
     winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
+    Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
+
+# Process Chocolatey Items
+foreach($App in $ChocolateyVDIRemote){
+    Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
+    # Added --silent and --accept-package-agreements for better automation
+    choco install --id $($App.ID) -y
     Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
 }
