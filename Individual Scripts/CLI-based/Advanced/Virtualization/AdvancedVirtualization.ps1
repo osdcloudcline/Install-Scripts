249281ec59359edbd9d1977aa745461f7b4d054d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-20 08:52:49 -0400

    Add download and extraction for VMware Workstation

--- Individual Scripts/CLI-based/Advanced/Virtualization/AdvancedVirtualization.ps1
+++ Individual Scripts/CLI-based/Advanced/Virtualization/AdvancedVirtualization.ps1
@@ -9,3 +9,11 @@ $app1 = "VMWare Workstation Professional 25H2"
 
 $VMWareWSProDLURL = "https://dl.bobpony.com/software/vmware/workstation/VMware-Workstation-Full-25H2-24995812.zip"
 $destination = "C:\downloads"
+
+
+Write-Host "Downloading: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan  
+Save-WebFile -SourceUrl $VMWareWSProDLURL -DestinationDirectory $destination
+
+Write-Host 
+Write-Host "Extracting: $app1 ..." -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -ForegroundColor Cyan 
+Expand-7Zip -ArchiveFileName "C:\downloads\VMware-Workstation-Full-25H2-24995812.zip" -TargetPath $destination
