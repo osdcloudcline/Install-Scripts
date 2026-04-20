d0f5307d8e400604f411713aa514e3065f61171d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-20 08:52:23 -0400

    Add VMware Workstation download URL and destination

--- Individual Scripts/CLI-based/Advanced/Virtualization/AdvancedVirtualization.ps1
+++ Individual Scripts/CLI-based/Advanced/Virtualization/AdvancedVirtualization.ps1
@@ -5,5 +5,7 @@ Write-Host "Processing Windows Feature: Windows Sandbox" -ForegroundColor Cyan
 Write-Host
 Enable-WindowsOptionalFeature -Online -FeatureName "Containers-DisposableClientVM" -All
 
+$app1 = "VMWare Workstation Professional 25H2"
+
 $VMWareWSProDLURL = "https://dl.bobpony.com/software/vmware/workstation/VMware-Workstation-Full-25H2-24995812.zip"
 $destination = "C:\downloads"
