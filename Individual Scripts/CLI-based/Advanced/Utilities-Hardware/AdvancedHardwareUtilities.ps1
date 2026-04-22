c2ff28e2b0713947598a7f8542ec97db3e565cdc
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-22 11:01:19 -0400

    Add silent and agreement options for installations
    Enhanced installation commands for better automation.

--- Individual Scripts/CLI-based/Advanced/Utilities-Hardware/AdvancedHardwareUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Hardware/AdvancedHardwareUtilities.ps1
@@ -20,3 +20,11 @@ foreach($App in $ADVHWUtils){
     winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
     Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
 }
+
+# Process Chocolatey Items
+foreach($App in $ADVChocoHWUtils){
+    Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
+    # Added --silent and --accept-package-agreements for better automation
+    choco install $($App.ID) -y
+    Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
