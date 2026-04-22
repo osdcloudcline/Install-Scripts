e7d4a63e80c851b6af14fd038335d278e96c6836
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-22 13:45:21 -0400

    Update AdvancedSWUtilities.ps1

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
@@ -1,1 +1,14 @@
+# Define the Advanced Software Utilities Software using Hash Tables @{ }
+$ADVSWUtils = @(
+   @{ Name = "7-Zip"; ID = "Crucial.StorageExecutive" }
+   @{ Name = "CCleaner"; ID = "Logitech.Options" }
+   @{ Name = "DDU"; ID = "Logitech.UnifyingSoftware" }
+   @{ Name = "Glary Utilities"; ID = "Logitech.GHUB" }
+   @{ Name = "Hardware Monitor Pro; ID = "Logitech.LGS" }
+   @{ Name = "Microsoft Deployment Toolkit"; ID = "Microsoft.MouseandKeyboardCenter" }
+   @{ Name = "Microsoft DaRT"; ID = "Microsoft.MouseandKeyboardCenter" }
+   @{ Name = "Smart Defrag"; ID = "Microsoft.MouseandKeyboardCenter" }
+   @{ Name = "Windows 11 ADK 26H1"; ID = "Microsoft.MouseandKeyboardCenter" }
+   @{ Name = "Wise Registry Cleaner"; ID = "Microsoft.MouseandKeyboardCenter" }
+)
 
