459199bfca16480211baf4a00cdb9d2b15882732
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-22 13:52:51 -0400

    Implement download and installation for MS DaRT
    Added functionality to download and install MS DaRT setup file.

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
@@ -48,3 +48,14 @@ Save-WebFile -SourceUrl $MDTKUrl -DestinationDirectory $OSDCloudGHdownloads
 
 Start-Process -FilePath "C:\downloads\MicrosoftDeploymentToolkit_x64.msi" -ArgumentList "/quiet /norestart"
 
+$app1 = "MS DART"
+
+Write-Host
+Write-Verbose "Acquiring $app1 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose 
+$OSDCloudGHdownloads = "C:\downloads"
+$MSDARTEXE = "C:\downloads\MSDaRT100.msi"
+$DARTUrl = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/DaRT/version%2010.0%20x64/MSDaRT100.msi"
+Write-Verbose "Processing and Downloading: $app1 Setup File..." -Verbose
+Save-WebFile -SourceUrl $DARTUrl -DestinationDirectory $OSDCloudGHdownloads
+
+Start-Process -FilePath "C:\downloads\MSDaRT100.msi" 
