39a4398baa01813c4d32acc09b561cb58beac7e5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-22 13:49:42 -0400

    Implement Glary Utilities installation and setup
    Added functionality to download and install Glary Utilities, including unzipping and moving files to the appropriate directories.

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
@@ -19,3 +19,20 @@ foreach($App in $ADVSWUtils){
     winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
     Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
 }
+
+$OSDCloudGHdownloads = "C:\downloads"
+$GlaryZipURL1 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/Glary%20Utilities/GlaryUtilities1.zip"
+$GlaryZipURL2 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/Glary%20Utilities/GlaryUtilities2.zip"
+$GlaryUnZipPath = "C:\downloads\GlaryUtilities"
+
+Save-WebFile -SourceUrl $GlaryZipURL1 -DestinationDirectory $OSDCloudGHdownloads
+Save-WebFile -SourceUrl $GlaryZipURL2 -DestinationDirectory $OSDCloudGHdownloads
+Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities1.zip" -TargetPath $GlaryUnZipPath 
+Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\GlaryUtilities2.zip" -TargetPath $GlaryUnZipPath 
+Copy-Item -Path "C:\downloads\GlaryUtilities" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
+Stop-Process -Name Integrator.exe -Force
+Stop-Process -Name MemfilesService.exe -Force
+Copy-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities\*.*" -Destination "C:\Program Files (x86)\Glary Utilities" -Recurse -Force 
+Remove-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities" -Force -Confirm:$false
+Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Glary Utilities.lnk" -Destination "C:\Users\$env:username\Desktop" -Force
+
