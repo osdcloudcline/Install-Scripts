b33d78687422e3a90f87bb7b3e16abb9c6ffd76d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-22 14:10:25 -0400

    Implement download and installation for Windows SDK and ADK
    Added functionality to download and install Windows SDK and ADK setup files from GitHub.

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
@@ -36,6 +36,45 @@ Copy-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities\*.*" -Des
 Remove-Item -Path "C:\Program Files (x86)\Glary Utilities\GlaryUtilities" -Force -Confirm:$false
 Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Glary Utilities.lnk" -Destination "C:\Users\$env:username\Desktop" -Force
 
+$app1 = "Windows 11 22H2 SDK"
+$app2 = "Windows 11 26H1 ADK"
+$app3 = "Windows 11 26H1 ADK PE Add-On"
+
+
+Write-Host
+Write-Verbose "Acquiring $app1 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose 
+$OSDCloudGHdownloads = "C:\downloads"
+$Win11_22H2SDK = "C:\downloads\winsdksetup.exe"
+$Win11_22H2SDKUrl = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/OS%20Kits/winsdksetup.exe"
+Write-Verbose "Processing and Downloading: $app1 Setup File..." -Verbose
+Save-WebFile -SourceUrl $Win11_22H2SDKUrl -DestinationDirectory $OSDCloudGHdownloads
+
+Start-Process -FilePath "C:\OSDCloud\GitHub\downloads\winsdksetup.exe" -ArgumentList "/quiet /norestart"
+
+pause
+
+Write-Host
+Write-Verbose "Acquiring $app2 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose 
+$OSDCloudGHdownloads = "C:\downloads"
+$Win11_26H1ADK = "C:\downloads\adksetup.exe"
+$Win11_26H1ADKUrl = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/OS%20Kits/adksetup.exe"
+Write-Verbose "Processing and Downloading: $app2 Setup File..." -Verbose
+Save-WebFile -SourceUrl $Win11_26H1ADKUrl -DestinationDirectory $OSDCloudGHdownloads
+
+Start-Process -FilePath "C:\downloads\adksetup.exe" -ArgumentList "/quiet /norestart"
+
+pause
+
+Write-Host
+Write-Verbose "Acquiring $app3 setup file from OSDCloudCline GitHub OSDCloud\OS Kits repository...." -Verbose
+$OSDCloudGHdownloads = "C:\downloads"
+$Win11_26H1PEADK = "C:\downloads\adkwinpesetup.exe"
+$Win11_26H1PEADKUrl = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/OS%20Kits/adkwinpesetup.exe"
+Write-Verbose "Processing and Downloading: $app3 Setup File..." -Verbose
+Save-WebFile -SourceUrl $Win11_26H1PEADKUrl -DestinationDirectory $OSDCloudGHdownloads
+
+Start-Process -FilePath "C:\downloads\adkwinpesetup.exe" -ArgumentList "/quiet /norestart"
+
 $app1 = "Microsoft Deployment Toolkit Build 8456"
 
 Write-Host
