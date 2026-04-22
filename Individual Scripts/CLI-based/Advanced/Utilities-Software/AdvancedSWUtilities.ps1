56d739c6f795e22764ec2e463341288392793d34
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-22 14:36:53 -0400

    Update software list in AdvancedSWUtilities.ps1
    Removed outdated software entries and updated IDs for Smart Defrag and Wise Registry Cleaner.

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
@@ -4,12 +4,8 @@ $ADVSWUtils = @(
    @{ Name = "CCleaner"; ID = "Piriform.CCleaner" }
    @{ Name = "DDU"; ID = "Wagnardsoft.DisplayDriverUninstaller" }
    @{ Name = "Glary Utilities"; ID = "Glarysoft.GlaryUtilities" }
-   @{ Name = "Hardware Monitor Pro"; ID = "Logitech.LGS" }
-   @{ Name = "Microsoft Deployment Toolkit"; ID = "Microsoft.MouseandKeyboardCenter" }
-   @{ Name = "Microsoft DaRT"; ID = "Microsoft.MouseandKeyboardCenter" }
-   @{ Name = "Smart Defrag"; ID = "Microsoft.MouseandKeyboardCenter" }
-   @{ Name = "Windows 11 ADK 26H1"; ID = "Microsoft.MouseandKeyboardCenter" }
-   @{ Name = "Wise Registry Cleaner"; ID = "Microsoft.MouseandKeyboardCenter" }
+   @{ Name = "Smart Defrag"; ID = "IObit.SmartDefrag" }
+   @{ Name = "Wise Registry Cleaner"; ID = "WiseCleaner.WiseRegistryCleaner" }
 )
 
 # Process WinGet Items
