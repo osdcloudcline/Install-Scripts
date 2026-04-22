8b9f0aab6bcb9e5f88f8d713415d592373f60884
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-22 09:01:22 -0400

    Implement Advanced Hardware Utilities with WinGet
    Added advanced hardware utilities for installation using WinGet.

--- Individual Scripts/CLI-based/Advanced/Utilities-Hardware/AdvancedHardwareUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Hardware/AdvancedHardwareUtilities.ps1
@@ -1,1 +1,18 @@
+# Define the Advanced Hardware Utilities Software using Hash Tables @{ }
+$ADVHWUtils = @(
+   @{ Name = "Crucial Storage"; ID = "" }
+   @{ Name = "Logitech Options"; ID = "" }
+   @{ Name = "Logitech Unifying Software"; ID = "" }
+   @{ Name = "Microsoft Keyboard and Mouse"; ID = "" }
+   @{ Name = "Samsung Magician"; ID = "" }
+   @{ Name = "Western Digital SSD"; ID = "" }
+)
 
+
+# Process WinGet Items
+foreach($App in $ADVHWUtils){
+    Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
+    # Added --silent and --accept-package-agreements for better automation
+    winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
+    Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
