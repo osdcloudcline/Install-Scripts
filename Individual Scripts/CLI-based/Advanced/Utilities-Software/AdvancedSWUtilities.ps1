dc2ab6268ab8be3dbcabe233ee3f58814a1e7b11
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-22 13:48:40 -0400

    Revise software utilities and enhance installation process
    Updated software IDs for installation and improved automation.

--- Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
+++ Individual Scripts/CLI-based/Advanced/Utilities-Software/AdvancedSWUtilities.ps1
@@ -1,9 +1,9 @@
 # Define the Advanced Software Utilities Software using Hash Tables @{ }
 $ADVSWUtils = @(
-   @{ Name = "7-Zip"; ID = "Crucial.StorageExecutive" }
-   @{ Name = "CCleaner"; ID = "Logitech.Options" }
-   @{ Name = "DDU"; ID = "Logitech.UnifyingSoftware" }
-   @{ Name = "Glary Utilities"; ID = "Logitech.GHUB" }
+   @{ Name = "7-Zip"; ID = "7zip.7zip" }
+   @{ Name = "CCleaner"; ID = "Piriform.CCleaner" }
+   @{ Name = "DDU"; ID = "Wagnardsoft.DisplayDriverUninstaller" }
+   @{ Name = "Glary Utilities"; ID = "Glarysoft.GlaryUtilities" }
    @{ Name = "Hardware Monitor Pro; ID = "Logitech.LGS" }
    @{ Name = "Microsoft Deployment Toolkit"; ID = "Microsoft.MouseandKeyboardCenter" }
    @{ Name = "Microsoft DaRT"; ID = "Microsoft.MouseandKeyboardCenter" }
@@ -12,3 +12,10 @@ $ADVSWUtils = @(
    @{ Name = "Wise Registry Cleaner"; ID = "Microsoft.MouseandKeyboardCenter" }
 )
 
+# Process WinGet Items
+foreach($App in $ADVSWUtils){
+    Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
+    # Added --silent and --accept-package-agreements for better automation
+    winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
+    Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
