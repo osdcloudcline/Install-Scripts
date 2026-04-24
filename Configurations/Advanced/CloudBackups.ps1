b5249154fb9868443991276163960c4f2e8f1bb1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-24 13:35:38 -0400

    Add CloudBackups.ps1 for automated cloud backup installs
    This script defines cloud backup software options and automates their installation using WinGet.

---
+++ Configurations/Advanced/CloudBackups.ps1
@@ -0,0 +1,21 @@
+# Define the Advanced Cloud Backups Software using Hash Tables @{ }
+$ADVCloudBackups = @(
+   @{ Name = "Dropbox"; ID = "Dropbox.Dropbox" }
+   @{ Name = "Google Drive"; ID = "Google.GoogleDrive" } 
+   @{ Name = "NextCloud Desktop Client"; ID = "Nextcloud.NextcloudDesktop" }
+   @{ Name = "Microsoft OneDrive"; ID = "Microsoft.OneDrive" }
+   @{ Name = "Apple iCloud"; ID = "Apple.iCloud" }
+)
+
+
+
+# Process WinGet Items
+foreach($App in $ADVCloudBackups){
+    Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
+    # Added --silent and --accept-package-agreements for better automation
+    winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
+    Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
+
+$UPBR = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Individual%20Scripts/CLI-based/Advanced/Cloud%20Backups/UPBR.ps1")
+Invoke-Expression $($UPBR.Content)
