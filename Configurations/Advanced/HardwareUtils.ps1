279db3e4ca26277e6654eecd1739b4d86b51f25f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-24 13:40:07 -0400

    Add Advanced Hardware Utilities PowerShell script

---
+++ Configurations/Advanced/HardwareUtils.ps1
@@ -0,0 +1,30 @@
+# Define the Advanced Hardware Utilities Software using Hash Tables @{ }
+$ADVHWUtils = @(
+   @{ Name = "Crucial Storage"; ID = "Crucial.StorageExecutive" }
+   @{ Name = "Logitech Options"; ID = "Logitech.Options" }
+   @{ Name = "Logitech Unifying Software"; ID = "Logitech.UnifyingSoftware" }
+   @{ Name = "Logitech G-Hub"; ID = "Logitech.GHUB" }
+   @{ Name = "Logitech Gaming Software"; ID = "Logitech.LGS" }
+   @{ Name = "Microsoft Keyboard and Mouse"; ID = "Microsoft.MouseandKeyboardCenter" }
+)
+$ADVChocoHWUtils = @(
+   @{ Name = "Samsung Magician"; ID = "samsung-magician" }
+   @{ Name = "Western Digital SSD"; ID = "data-lifeguard-diagnostic --version=1.36" }
+)
+
+
+# Process WinGet Items
+foreach($App in $ADVHWUtils){
+    Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
+    # Added --silent and --accept-package-agreements for better automation
+    winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
+    Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
+
+# Process Chocolatey Items
+foreach($App in $ADVChocoHWUtils){
+    Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
+    # Added --silent and --accept-package-agreements for better automation
+    choco install $($App.ID) -y
+    Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
