8f54f185c3f602892693355431983d4a1b1508a3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-24 16:45:10 -0400

    Add source option to winget install command

--- Configurations/Advanced/CloudBackups.ps1
+++ Configurations/Advanced/CloudBackups.ps1
@@ -13,7 +13,7 @@ $ADVCloudBackups = @(
 foreach($App in $ADVCloudBackups){
     Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
     # Added --silent and --accept-package-agreements for better automation
-    winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
+    winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force --source winget
     Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
 }
 
