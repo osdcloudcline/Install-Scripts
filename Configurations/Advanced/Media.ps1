705f5c60d0eaff7528155f1b2a6c896cf402c630
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-24 13:38:28 -0400

    Create Media.ps1 for advanced media software installation
    Added a PowerShell script to define and install advanced media software using WinGet.

---
+++ Configurations/Advanced/Media.ps1
@@ -0,0 +1,30 @@
+# Define the Advanced Media Software using Hash Tables @{ }
+$ADVMedia = @(
+   @{ Name = "Audacity"; ID = "Audacity.Audacity" }
+   @{ Name = "FFMPEG for Audacity"; ID = "BtbN.FFmpeg.GPL.Shared.8.1" }
+   @{ Name = "GIMP"; ID = "GIMP.GIMP.3" }
+   @{ Name = "Handbrake"; ID = "HandBrake.HandBrake" }
+   @{ Name = "Kodi"; ID = "XBMCFoundation.Kodi" }
+   @{ Name = "MKV Toolnix"; ID = "MoritzBunkus.MKVToolNix" }
+   @{ Name = "MakeMKV"; ID = "GuinpinSoft.MakeMKV" }
+   @{ Name = "MediaInfo"; ID = "MediaArea.MediaInfo.GUI" }
+   @{ Name = "Logitech Harmony Universal Remote Control"; ID = "Logitech.MyHarmony" }
+   @{ Name = "PowerISO"; ID = "PowerSoftware.PowerISO" }
+   @{ Name = "VLC Player"; ID = "VideoLAN.VLC" }
+   @{ Name = "Apple iTunes"; ID = "Apple.iTunes" }
+   @{ Name = "Apple Application Support 64-bit"; ID = "Apple.AppleApplicationSupport.x64" }
+   @{ Name = "Apple Mobile Device Support"; ID = "Apple.AppleMobileDeviceSupport" }
+   @{ Name = "Apple Bonjour"; ID = "Apple.Bonjour" }
+)
+
+
+# Process WinGet Items
+foreach($App in $ADVMedia){
+    Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
+    # Added --silent and --accept-package-agreements for better automation
+    winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force
+    Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
+
+$HDVideoFactory = Invoke-WebRequest("https://github.com/osdcloudcline/Install-Scripts/raw/refs/heads/main/Individual%20Scripts/CLI-based/Advanced/Media/HDVideoConverterPro.ps1")
+Invoke-Expression $($HDVideoFactory.Content)
