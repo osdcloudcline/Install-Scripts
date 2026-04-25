d500fc84f66c6338c53291325207ac4580693f7f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-25 07:49:53 -0400

    Add download process for Norton 360

--- Configurations/Advanced/Security.ps1
+++ Configurations/Advanced/Security.ps1
@@ -10,3 +10,11 @@ Write-Verbose "Processing $app1 for download..." -Verbose
 Save-WebFile -SourceUrl $BDURLDL -DestinationDirectory $destination
 
 
+$app2 = "Norton 360"
+
+$N360DLURL = "https://github.com/osdcloudcline/Software/raw/main/Security/Norton%20360/N360Downloader.exe"
+
+$destination = "C:\downloads"
+
+Write-Verbose "Processing $app2 for download..." -Verbose
+Save-WebFile -SourceUrl $N360DLURL -DestinationDirectory $destination
