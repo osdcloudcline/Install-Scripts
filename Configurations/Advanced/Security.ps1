5ab857ab389d05d3b282f4f9420bccf2edb62b3d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-25 07:47:38 -0400

    Add Bitdefender Total Security download script

--- Configurations/Advanced/Security.ps1
+++ Configurations/Advanced/Security.ps1
@@ -1,1 +1,12 @@
+$app1 = "Bitdefender Total Security"
+
+Import-Module -Name OSD -Force
+
+$BDURLDL = "https://github.com/osdcloudcline/Software/raw/main/Security/BitDefender/bitdefender_tsecurity.exe"
+
+$destination = "C:\downloads"
+
+Write-Verbose "Processing $app1 for download..." -Verbose
+Save-WebFile -SourceUrl $BDURLDL -DestinationDirectory $destination
+
 
