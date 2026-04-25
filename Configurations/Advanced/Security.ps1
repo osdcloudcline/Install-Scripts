34a5bba9d0ed299bd01e5ccfcd12573381079e9e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-25 07:56:23 -0400

    Add Nord VPN to security applications list
    Added Nord VPN to the list of applications and download URLs. Updated download processing for the new application.

--- Configurations/Advanced/Security.ps1
+++ Configurations/Advanced/Security.ps1
@@ -2,31 +2,34 @@ $app1 = "Bitdefender Total Security"
 $app2 = "Norton 360"
 $app3 = "MalwareBytes"
 $app4 = "IP Vanish VPN"
-
+$app5 = "Nord VPN"
 
 $BDURLDL = "https://github.com/osdcloudcline/Software/raw/main/Security/BitDefender/bitdefender_tsecurity.exe"
 $N360DLURL = "https://github.com/osdcloudcline/Software/raw/main/Security/Norton%20360/N360Downloader.exe"
 $MBDLURL = "https://github.com/osdcloudcline/Software/raw/main/Security/MalwareBytes/MBSetup.exe"
 $VPNURL = "https://www.ipvanish.com/software/setup-prod-v2/ipvanish-setup.exe"
+$VPNURL = "https://downloads.nordcdn.com/apps/windows/NordVPN/latest/NordVPNSetup.exe"
 
 Import-Module -Name OSD -Force
-
-
 
 $destination = "C:\downloads"
 
 Write-Verbose "Processing $app1 for download..." -Verbose
 Save-WebFile -SourceUrl $BDURLDL -DestinationDirectory $destination
-
-
+Write-Host
 
 Write-Verbose "Processing $app2 for download..." -Verbose
 Save-WebFile -SourceUrl $N360DLURL -DestinationDirectory $destination
-
+Write-Host
 
 Write-Verbose "Processing $app3 for download..." -Verbose
 Save-WebFile -SourceUrl $MBDLURL -DestinationDirectory $destination
+Write-Host
 
-
-Write-Verbose "Processing $app1 for download..." -Verbose
+Write-Verbose "Processing $app4 for download..." -Verbose
 Save-WebFile -SourceUrl $VPNURL -DestinationDirectory $destination
+Write-Host
+
+Write-Verbose "Processing $app5 for download..." -Verbose
+Save-WebFile -SourceUrl $VPNURL -DestinationDirectory $destination
+Write-Host
