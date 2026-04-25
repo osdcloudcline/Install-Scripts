5d05085e842b310d03ca4d6a00eb27306af440fc
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-25 08:05:44 -0400

    Add Hiren's BootCD PE download to SystemRescue.ps1

--- Configurations/Advanced/SystemRescue.ps1
+++ Configurations/Advanced/SystemRescue.ps1
@@ -2,11 +2,14 @@ Import-Module -Name OSD -Force
 
 
 $ClonezillaURL = "https://free.nchc.org.tw/clonezilla-live/stable/clonezilla-live-3.3.1-35-amd64.iso"
-
+$HirenBootCDPEURL = "https://www.hirensbootcd.org/files/HBCD_PE_x64.iso"
 
 $DLDestination = "C:\downloads\SystemRescue"
 
 
 Write-Verbose "Processing: Downloading Clonezilla x64 3.3.1.35..."
-
 Save-WebFile -SourceUrl $ClonezillaURL -DestinationDirectory $DLDestination
+Write-Host
+
+Write-Verbose "Processing: Downloading Hiren's BootCD PE x64..."
+Save-WebFile -SourceUrl $HirenBootCDPEURL -DestinationDirectory $DLDestination
