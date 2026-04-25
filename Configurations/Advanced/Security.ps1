02c54f09be24e4228d3b43b4ea8e625e67409abe
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-25 07:53:56 -0400

    Refactor Security.ps1 to streamline downloads
    Removed redundant variable assignments and download processing for Norton 360 and MalwareBytes. Added download processing for IP Vanish VPN.

--- Configurations/Advanced/Security.ps1
+++ Configurations/Advanced/Security.ps1
@@ -1,8 +1,17 @@
 $app1 = "Bitdefender Total Security"
+$app2 = "Norton 360"
+$app3 = "MalwareBytes"
+$app4 = "IP Vanish VPN"
+
+
+$BDURLDL = "https://github.com/osdcloudcline/Software/raw/main/Security/BitDefender/bitdefender_tsecurity.exe"
+$N360DLURL = "https://github.com/osdcloudcline/Software/raw/main/Security/Norton%20360/N360Downloader.exe"
+$MBDLURL = "https://github.com/osdcloudcline/Software/raw/main/Security/MalwareBytes/MBSetup.exe"
+$VPNURL = "https://www.ipvanish.com/software/setup-prod-v2/ipvanish-setup.exe"
 
 Import-Module -Name OSD -Force
 
-$BDURLDL = "https://github.com/osdcloudcline/Software/raw/main/Security/BitDefender/bitdefender_tsecurity.exe"
+
 
 $destination = "C:\downloads"
 
@@ -10,20 +19,14 @@ Write-Verbose "Processing $app1 for download..." -Verbose
 Save-WebFile -SourceUrl $BDURLDL -DestinationDirectory $destination
 
 
-$app2 = "Norton 360"
-
-$N360DLURL = "https://github.com/osdcloudcline/Software/raw/main/Security/Norton%20360/N360Downloader.exe"
-
-$destination = "C:\downloads"
 
 Write-Verbose "Processing $app2 for download..." -Verbose
 Save-WebFile -SourceUrl $N360DLURL -DestinationDirectory $destination
 
-$app3 = "MalwareBytes"
-
-$MBDLURL = "https://github.com/osdcloudcline/Software/raw/main/Security/MalwareBytes/MBSetup.exe"
-
-$destination = "C:\downloads"
 
 Write-Verbose "Processing $app3 for download..." -Verbose
 Save-WebFile -SourceUrl $MBDLURL -DestinationDirectory $destination
+
+
+Write-Verbose "Processing $app1 for download..." -Verbose
+Save-WebFile -SourceUrl $VPNURL -DestinationDirectory $destination
