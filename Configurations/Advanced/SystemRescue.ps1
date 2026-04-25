d6dedfbbec8852dbe547734af3d8a11eeb7f931c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-25 08:04:28 -0400

    Add Clonezilla download script to SystemRescue.ps1

--- Configurations/Advanced/SystemRescue.ps1
+++ Configurations/Advanced/SystemRescue.ps1
@@ -1,1 +1,12 @@
+Import-Module -Name OSD -Force
 
+
+$ClonezillaURL = "https://free.nchc.org.tw/clonezilla-live/stable/clonezilla-live-3.3.1-35-amd64.iso"
+
+
+$DLDestination = "C:\downloads\SystemRescue"
+
+
+Write-Verbose "Processing: Downloading Clonezilla x64 3.3.1.35..."
+
+Save-WebFile -SourceUrl $ClonezillaURL -DestinationDirectory $DLDestination
