7c87521759f3e90d1fc28888a89e8d5485149fe4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-04-30 00:34:04 -0400

    Remove duplicate entries in ADVMedia array

--- Configurations/Advanced/Media.ps1
+++ Configurations/Advanced/Media.ps1
@@ -1,29 +1,27 @@
 # Define the Advanced Media Software using Hash Tables @{ }
 $ADVMedia = @(
-   @{ Name = "Audacity"; ID = "Audacity.Audacity" }
-   @{ Name = "FFMPEG for Audacity"; ID = "BtbN.FFmpeg.GPL.Shared.8.1" }
-   @{ Name = "GIMP"; ID = "GIMP.GIMP.3" }
-   @{ Name = "Handbrake"; ID = "HandBrake.HandBrake" }
-   @{ Name = "Kodi"; ID = "XBMCFoundation.Kodi" }
-   @{ Name = "MKV Toolnix"; ID = "MoritzBunkus.MKVToolNix" }
-   @{ Name = "MakeMKV"; ID = "GuinpinSoft.MakeMKV" }
-   @{ Name = "MediaInfo"; ID = "MediaArea.MediaInfo.GUI" }
-   @{ Name = "Media Monkey 2024"; ID = "VentisMedia.MediaMonkey.2024" }
-   @{ Name = "Exact Audio Copy"; ID = "AndreWiethoff.ExactAudioCopy" }
-   @{ Name = "Logitech Harmony Universal Remote Control"; ID = "Logitech.MyHarmony" }
-   @{ Name = "PowerISO"; ID = "PowerSoftware.PowerISO" }
-   @{ Name = "VLC Player"; ID = "VideoLAN.VLC" }
-   @{ Name = "Apple iTunes"; ID = "Apple.iTunes" }
-   @{ Name = "Apple Application Support 64-bit"; ID = "Apple.AppleApplicationSupport.x64" }
-   @{ Name = "Apple Mobile Device Support"; ID = "Apple.AppleMobileDeviceSupport" }
+   @{ Name = "Audacity"; ID = "Audacity.Audacity" },
+   @{ Name = "FFMPEG for Audacity"; ID = "BtbN.FFmpeg.GPL.Shared.8.1" },
+   @{ Name = "GIMP"; ID = "GIMP.GIMP.3" },
+   @{ Name = "Handbrake"; ID = "HandBrake.HandBrake" },
+   @{ Name = "Kodi"; ID = "XBMCFoundation.Kodi" },
+   @{ Name = "MKV Toolnix"; ID = "MoritzBunkus.MKVToolNix" },
+   @{ Name = "MakeMKV"; ID = "GuinpinSoft.MakeMKV" },
+   @{ Name = "MediaInfo"; ID = "MediaArea.MediaInfo.GUI" },
+   @{ Name = "Media Monkey 2024"; ID = "VentisMedia.MediaMonkey.2024" },
+   @{ Name = "Exact Audio Copy"; ID = "AndreWiethoff.ExactAudioCopy" },
+   @{ Name = "Logitech Harmony Universal Remote Control"; ID = "Logitech.MyHarmony" },
+   @{ Name = "PowerISO"; ID = "PowerSoftware.PowerISO" },
+   @{ Name = "VLC Player"; ID = "VideoLAN.VLC" },
+   @{ Name = "Apple iTunes"; ID = "Apple.iTunes" },
+   @{ Name = "Apple Application Support 64-bit"; ID = "Apple.AppleApplicationSupport.x64" },
+   @{ Name = "Apple Mobile Device Support"; ID = "Apple.AppleMobileDeviceSupport" },
    @{ Name = "Apple Bonjour"; ID = "Apple.Bonjour" }
 )
-
 
 # Process WinGet Items
 foreach($App in $ADVMedia){
     Write-Host "`nProcessing install for: $($App.Name)" -ForegroundColor Cyan
-    # Added --silent and --accept-package-agreements for better automation
     winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force --source winget
     Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
 }
