0b5cb49e56d539ddcd567dd2138a97faf7293f96
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-05-02 22:49:59 -0400

    Enhance OEMConfigurations.ps1 for Dell applications
    Added Dell-specific applications to the OEM configuration script and removed redundant write-host statement.

--- Configurations/Advanced/OEMConfigurations.ps1
+++ Configurations/Advanced/OEMConfigurations.ps1
@@ -4,12 +4,31 @@ $PCManufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
 If($PCManufacturer -like "*Dell*"){
 
 $DellApps = @(
- @{Name=""; ID="" }
+ @{Name="Dell Command | Update"; ID="Dell.CommandUpdate" },
+ @{Name="Dell Command | Update for Windows Universal"; ID="Dell.CommandUpdate.Universal" },
+ @{Name="Dell Optimizer"; ID="Dell.Optimizer" },
+ @{Name="Dell System Update"; ID="Dell.SystemUpdate" },
+ @{Name="Dell Command | Configure"; ID="Dell.CommandConfigure" },
+ @{Name="Dell Display and Peripheral Manager"; ID="Dell.DisplayAndPeripheralManager" },
+ @{Name="Dell Display Manager"; ID="Dell.DisplayManager" },
+ @{Name="Dell Peripheral Manager"; ID="Dell.PeripheralManager" }
+ 
+ foreach($App in $DellApps){
+ Write-Host "Dell System Detected. Processing Dell-specific tools..." -ForegroundColor Yellow
+
+ }
 )
 
-Write-Host "Dell System Detected. Processing Dell-specific tools..." -ForegroundColor Yellow
 
 
 
 
-}
+
+}elseif($PCManufacturer -like "*HP*"){
+
+$HPApps = @(
+  @{Name= ""; ID="" }
+)
+
+
+Write-Host "HP System Detected. Processing HP-specific tools..." -ForegroundColor Yellow
