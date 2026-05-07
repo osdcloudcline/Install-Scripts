3355d2d35abe6274c732d8dce5fb8a8ebf3b1b8a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-05-07 07:25:30 -0400

    Fix formatting and install commands for Dell and HP apps

--- Configurations/Advanced/OEMConfigurations.ps1
+++ Configurations/Advanced/OEMConfigurations.ps1
@@ -12,17 +12,13 @@ $DellApps = @(
  @{Name="Dell Display and Peripheral Manager"; ID="Dell.DisplayAndPeripheralManager" },
  @{Name="Dell Display Manager"; ID="Dell.DisplayManager" },
  @{Name="Dell Peripheral Manager"; ID="Dell.PeripheralManager" }
- 
+) 
  foreach($App in $DellApps){
  Write-Host "Dell System Detected. Processing Dell-specific tools..." -ForegroundColor Yellow
-
+     winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force --source winget
+ Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
  }
 )
-
-
-
-
-
 
 }elseif($PCManufacturer -like "*HP*"){
 
@@ -30,5 +26,9 @@ $HPApps = @(
   @{Name= ""; ID="" }
 )
 
-
+foreach($App in $HPApps){
 Write-Host "HP System Detected. Processing HP-specific tools..." -ForegroundColor Yellow
+      winget install --id $($App.ID) --exact --silent --accept-package-agreements --accept-source-agreements --force --source winget
+Write-Host "Completed: $($App.Name) install`n" -ForegroundColor Green
+}
+}
