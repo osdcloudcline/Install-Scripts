4dba9d3036d2be36fb14644aa3d423aae67cad60
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-07-25 18:00:43 -0400

    Modify Install-PackageProvider command in Install.ps1
    Removed the '-Verbose' flag from the Install-PackageProvider command.

--- PowerShell Modules/Package Providers/Install.ps1
+++ PowerShell Modules/Package Providers/Install.ps1
@@ -11,8 +11,10 @@ $providers = @('NuGet',
 foreach($provider in $providers){
 Write-Host "Installing Windows PowerShell Package Provider: $provider" -ForegroundColor Cyan
 Write-Host
-            Install-PackageProvider -Name $provider -Force -Verbose -AllowClobber
+            Install-PackageProvider -Name $provider -Force 
 Write-Host
 Write-Host "Completed installation of Windows PowerShell Package Provider: $provider" -ForegroundColor Green
 
 }
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
