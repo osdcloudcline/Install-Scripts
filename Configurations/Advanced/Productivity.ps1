cc0cbb2fba2f51bd3400dfcc09145b16ab7eb84a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-07-25 18:32:43 -0400

    Implement Office 2024 installation script
    Added functionality to download and install Microsoft Office 2024.

--- Configurations/Advanced/Productivity.ps1
+++ Configurations/Advanced/Productivity.ps1
@@ -1,4 +1,20 @@
-$AdobeSetup = ""
+$app1 = "Microsoft Office 2024 LTSC"
 
-Start-Process -FilePath $AdobeSetup
+Write-Host "Processing: $app1..." -ForegroundColor Cyan
+
+$OfficeGHdownloads = "C:\Office\2024" 
+mkdir $OfficeGHdownloads
+$Office2024Files1Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/Configuration-Office2024-EntireSuiteVL.xml"
+$Office2024Files2Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/officedeploymenttool_18227-20162.exe"
+$Office2024Files3Url = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Productivity/Microsoft/Office%202024/setup.exe"
+
+
+Save-WebFile -SourceUrl $Office2024Files1Url -DestinationDirectory $OfficeGHdownloads
+Save-WebFile -SourceUrl $Office2024Files2Url -DestinationDirectory $OfficeGHdownloads
+Save-WebFile -SourceUrl $Office2024Files3Url -DestinationDirectory $OfficeGHdownloads
+
+$exe = "C:\Office\2024\setup.exe"
+$arguments = "/configure C:\Office\2024\Configuration-Office2024-EntireSuiteVL.xml"
+
+Start-Process -FilePath $exe -ArgumentList $arguments
 
